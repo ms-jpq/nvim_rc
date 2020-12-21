4c9fc4dc16403c8c35a880ec05528fd70b54d6f0
Fly me to the moon <git@bigly.dog>
2020-12-21 15:23:37 -0800

    wait for thread join

--- init.py
+++ init.py
@@ -27,29 +27,31 @@ def main() -> None:
     notif_q, req_q = NOTIF_Q(), RPC_Q()
 
     with attach("stdio") as nvim:
+        try:
+            th = Thread(target=srv, args=(nvim, notif_q, req_q))
 
-        th = Thread(target=srv, daemon=True, args=(nvim, notif_q, req_q))
+            def on_notif(event: str, *args: Any) -> None:
+                notif_q.put((event, args))
 
-        def on_notif(event: str, *args: Any) -> Any:
-            fut = Future[Any]()
-            notif_q.put((fut, (event, args)))
-            return fut.result()
+            def on_req(event: str, *args: Any) -> Any:
+                fut = Future[Any]()
+                req_q.put((fut, (event, args)))
+                return fut.result()
 
-        def on_req(event: str, *args: Any) -> None:
-            req_q.put((event, args))
+            def on_setup() -> None:
+                th.start()
 
-        def on_setup() -> None:
-            th.start()
+            def on_err(error: str) -> None:
+                print(error, file=stderr)
 
-        def on_err(error: str) -> None:
-            print(error, file=stderr)
-
-        nvim.run_loop(
-            err_cb=on_err,
-            setup_cb=on_setup,
-            notification_cb=on_notif,
-            request_cb=on_req,
-        )
+            nvim.run_loop(
+                err_cb=on_err,
+                setup_cb=on_setup,
+                notification_cb=on_notif,
+                request_cb=on_req,
+            )
+        finally:
+            th.join()
 
 
 main()
