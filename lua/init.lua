3dcfbc7c9cbdeb579cad240c3731fd6103b38564
Fly me to the moon <git@bigly.dog>
2020-12-22 09:49:47 -0800

    more init

--- lua/init.lua
+++ lua/init.lua
@@ -10,6 +10,11 @@ local on_stderr = function (_, msg)
   vim.api.nvim_out_write(table.concat(msg, "\n"))
 end
 
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
 local chan = vim.fn.jobstart(
   {py_main},
   { rpc = true, on_exit = on_exit, on_stderr = on_stderr }
@@ -22,6 +27,3 @@ end
 local request = function (name, args)
   return vim.rpcrequest(chan, name, args)
 end
-
-
-notify("EVENT", {"ARG1", "ARG2", "ARG3"})
