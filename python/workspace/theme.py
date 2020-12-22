a5bad624e18a474c3bdadef022d7906b83100576
Fly me to the moon <git@bigly.dog>
2020-12-22 10:25:24 -0800

    theme

---
+++ python/workspace/theme.py
@@ -0,0 +1,20 @@
+from ..registery import settings
+
+# use 256 colours
+settings["termguicolors"] = True
+# always show status line
+settings["laststatus"] = 2
+# always show issues column
+settings["signcolumn"] = "yes"
+# dont show eob lines
+settings["fillchars"] = "eob:\ "
+# always show tabline
+settings["showtabline"] = 2
+# show line count
+settings["number"] = True
+
+
+# show cursor
+settings["cursorline"] = True
+# constant cursor styling
+settings["guicursor"] = ""
