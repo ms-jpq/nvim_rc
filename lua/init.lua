93477b922feff6351675cabe63164ca382d40aac
Fly me to the moon <git@bigly.dog>
2020-12-21 18:47:14 -0800

    init lua

--- lua/init.lua
+++ lua/init.lua
@@ -2,16 +2,26 @@ local vim_home = vim.env["XDG_CONFIG_HOME"] .. "/nvim"
 local py_main = vim_home .. "/init.py"
 
 
-local on_stderr = function (err, msg)
-  vim.api.nvim_err_write(table.concat(msg, "\n") .. "\n")
+local on_exit = function (_, code)
+  vim.api.nvim_err_writeln("EXITED - " .. code)
 end
 
-local chan = vim.fn.jobstart({py_main}, { rpc = true, on_stderr = on_stderr })
+local on_stderr = function (_, msg)
+  vim.api.nvim_err_writeln(table.concat(msg, "\n"))
+end
+
+local chan = vim.fn.jobstart(
+  {py_main},
+  { rpc = true, on_exit = on_exit, on_stderr = on_stderr }
+)
 
 
 lv = {}
 lv.notify = function (event, args)
   vim.rpcnotify(chan, event, args)
+end
+lv.request = function (event, args)
+  return vim.rpcrequest(chan, evnt, args)
 end
 
 
