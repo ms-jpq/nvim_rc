6d5667c2a02b1b3bce435917539743e99727814e
Fly me to the moon <git@bigly.dog>
2020-12-21 20:58:01 -0800

    more int

--- python/nvim/__init__.py
+++ python/nvim/__init__.py
@@ -1,6 +1,15 @@
 from .autocmd import AutoCMD
 from .client import NOTIF_MSG, RPC_MSG, Client, run_client
 from .keymap import KeyMap
+from .lib import (
+    AtomicInstruction,
+    LockBroken,
+    async_call,
+    atomic,
+    buffer_lock,
+    print,
+    window_lock,
+)
 from .logging import log
 from .rpc import RPC
 from .settings import Settings
