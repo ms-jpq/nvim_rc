ae4a99a670d7dac966a5a289b3989c93f9aa550b
Fly me to the moon <git@bigly.dog>
2020-12-27 06:55:34 -0800

    thing

---
+++ python/lib/rtp.py
@@ -0,0 +1,24 @@
+from locale import strxfrm
+from pathlib import Path
+from typing import Iterable
+
+from pynvim import Nvim
+from std2.pathlib import walk
+
+from ..nvim.atomic import Atomic
+
+
+def rtp_packages(nvim: Nvim, plugins: Iterable[Path]) -> Atomic:
+    atomic = Atomic()
+    head, *tail = nvim.list_runtime_paths()
+    rtp = ",".join((head, *(map(str, plugins)), *tail))
+    atomic.command(f"set runtimepath={rtp}")
+
+    for path in plugins:
+        plug = path / "plugin"
+        if plug.exists():
+            scripts = (str(s) for s in walk(plug) if s.suffix == ".vim")
+            for script in sorted(scripts, key=strxfrm):
+                atomic.command("source", script)
+
+    return atomic
--- python/packages/git_rtp.py
+++ python/packages/git_rtp.py
@@ -1,35 +1,15 @@
-from locale import strxfrm
-from typing import Iterable
+from pathlib import Path, PurePath
+from urllib.parse import urlparse
 
-from pynvim import Nvim
-from std2.pathlib import walk
-from yaml import load
+
+from yaml import safe_load
 
 from ..consts import CONF_PACKAGES, VIM_DIR
-from ..nvim.atomic import Atomic
-from pathlib import PurePath, Path
-from urllib.parse import urlparse
 
 
 def _p_name(uri: str) -> Path:
     url = urlparse(uri).path
     return VIM_DIR / PurePath(url).stem
 
-
-def packages(nvim: Nvim) -> Atomic:
-    conf = load(CONF_PACKAGES.open())
-    plugins = tuple(_p_name(c["uri"]) for c in conf)
-
-    atomic = Atomic()
-    head, *tail = nvim.list_runtime_paths()
-    rtp = ",".join((head, *(map(str, plugins)), *tail))
-    atomic.command(f"set runtimepath={rtp}")
-
-    for path in plugins:
-        plug = path / "plugin"
-        if plug.exists():
-            for vim_script in walk(plug):
-                if vim_script.suffix == ".vim":
-                    atomic.command("source", str(vim_script))
-
-    return atomic
+conf = safe_load(CONF_PACKAGES.open())
+plugins = tuple(_p_name(c["uri"]) for c in conf)
