7bb57fec5b47df1262dd934d62e11eedf837fb69
Fly me to the moon <git@bigly.dog>
2020-12-27 08:53:06 -0800

    rtp

--- python/nvim/rtp.py
+++ python/nvim/rtp.py
@@ -1,11 +1,18 @@
 from locale import strxfrm
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Iterator
 
 from pynvim import Nvim
-from std2.pathlib import walk
 
 from ..nvim.atomic import Atomic
+
+
+def _walk(path: Path) -> Iterator[Path]:
+    for p in path.iterdir():
+        if p.is_dir():
+            yield from _walk(p)
+        elif p.is_file():
+            yield p
 
 
 def rtp_packages(nvim: Nvim, plugins: Iterable[Path]) -> Atomic:
@@ -17,7 +24,7 @@ def rtp_packages(nvim: Nvim, plugins: Iterable[Path]) -> Atomic:
     for path in plugins:
         plug = path / "plugin"
         if plug.exists():
-            scripts = (str(s) for s in walk(plug) if s.suffix == ".vim")
+            scripts = (str(s) for s in _walk(plug) if s.suffix == ".vim")
             for script in sorted(scripts, key=strxfrm):
                 atomic.command(f"source {script}")
 
