3c41a9c6987462738885cf4318f91ed96d07730e
Fly me to the moon <git@bigly.dog>
2020-12-27 06:59:51 -0800

    fix

--- python/registery.py
+++ python/registery.py
@@ -2,12 +2,13 @@ from typing import Sequence, Tuple
 
 from pynvim import Nvim
 
+from .lib.rtp import rtp_packages
 from .nvim.atomic import Atomic
 from .nvim.autocmd import AutoCMD
 from .nvim.keymap import Keymap
 from .nvim.rpc import RPC, RpcSpec
 from .nvim.settings import Settings
-from .packages.git_rtp import packages
+from .packages.git_rtp import plugins
 
 atomic = Atomic()
 autocmd = AutoCMD()
@@ -22,7 +23,7 @@ _atomic.set_var("maplocalleader", " ")
 
 
 def drain(nvim: Nvim) -> Tuple[Atomic, Sequence[RpcSpec]]:
-    a0 = packages(nvim)
+    a0 = rtp_packages(nvim, plugins=plugins)
     a1, s1 = autocmd.drain(nvim.channel_id)
     a2 = keymap.drain(nvim.channel_id, None)
     s2 = rpc.drain()
