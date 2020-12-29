f965729fd0d3aa26a5a3c167afbfa954d2d2e529
Fly me to the moon <git@bigly.dog>
2020-12-29 12:38:02 -0800

    mv

--- python/_registery.py
+++ python/_registery.py
@@ -1,5 +1,5 @@
-from .components import fmt, linter, lsp
-from .editor import auto_comp, auto_save, prettier, search, syntax, whitespace
+from .components import lsp
+from .editor import auto_comp, auto_save, linter, prettier, search, syntax, whitespace
 from .modes import command, normal, poly, terminal, visual
 from .version_control import git
 from .workspace import input, misc, navigation, theme, wm
