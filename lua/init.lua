9e93bc5ef8fbaf1c7694b716280186b8244ebbc6
Fly me to the moon <git@bigly.dog>
2020-12-28 20:49:04 -0800

    init.lua

--- lua/init.lua
+++ lua/init.lua
@@ -1,8 +1,19 @@
-local py_main = vim.api.nvim_list_runtime_paths()[1] .. "/init.py"
+local nvim_home = vim.api.nvim_list_runtime_paths()[1]
+local py_main = nvim_home .. "/init.py"
+local pip_home = nvim_home .. "vars/pip_modules"
+
+
+if vim.env["PYTHONPATH"] then
+  vim.env["PYTHONPATH"] =  pip_home .. ":" .. vim.env["PYTHONPATH"]
+else
+  vim.env["PYTHONPATH"] = pip_home
+end
+
 
 local on_exit = function (_, code)
   vim.api.nvim_err_writeln("EXITED - " .. code)
 end
+
 
 local on_stderr = function (_, msg)
   vim.api.nvim_out_write(table.concat(msg, "\n"))
