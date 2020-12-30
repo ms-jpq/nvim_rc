c2164773f0c2a4f43b6354561c2781cf04d70ac1
Fly me to the moon <git@bigly.dog>
2020-12-30 01:28:45 -0800

    added entire

---
+++ python/text_objects/entire.py
@@ -0,0 +1,20 @@
+from pynvim import Nvim
+from pynvim.api import Window, Buffer
+
+
+from ..registery import keymap, rpc
+
+
+@rpc(blocking=True)
+def _entire(nvim: Nvim) -> None:
+    buf: Buffer = nvim.api.get_current_buf()
+    count: int = nvim.api.buf_line_count(buf)
+    last_line: str = nvim.api.buf_get_line(buf, -2, -1, True)
+    nvim.funcs.setpos("'<", (buf.number, 1, 1, 0))
+    nvim.funcs.setpos("'<", (buf.number, count, len(last_line), 0))
+
+
+keymap.o("ie") << "<cmd>" + _entire.call_line() + "<cr>"
+keymap.o("ae") << "<cmd>" + _entire.call_line() + "<cr>"
+keymap.v("ie") << "<esc><cmd>" + _entire.call_line() + "<cr>"
+keymap.v("ae") << "<esc><cmd>" + _entire.call_line() + "<cr>"
