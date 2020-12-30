0a87819e95610ae8ec8e2db0a23d5ad0913d1d4d
Fly me to the moon <git@bigly.dog>
2020-12-30 00:31:02 -0800

    auto save with handle

--- python/editor/auto_save.py
+++ python/editor/auto_save.py
@@ -1,5 +1,10 @@
+from asyncio.events import Handle, get_running_loop
+from typing import Optional
+
 from pynvim.api.nvim import Nvim
-from ..registery import settings, autocmd
+from python.nvim.lib import async_call, go
+
+from ..registery import autocmd, settings
 
 # auto load changes
 settings["autoread"] = True
@@ -27,16 +32,27 @@ def _auto_save(nvim: Nvim) -> None:
     nvim.command("silent! wa")
 
 
+_handle: Optional[Handle] = None
+
+
 @autocmd(
     "CursorHold",
     "CursorHoldI",
     "TextChanged",
     "TextChangedI",
-    blocking=True,
+    blocking=False,
     modifiers=("*", "nested"),
 )
 def _smol_save(nvim: Nvim) -> None:
-    nvim.command("silent! wa")
+    global _handle
+    if _handle:
+        _handle.cancel()
+
+    def cont() -> None:
+        go(async_call(nvim, _auto_save, nvim))
+
+    loop = get_running_loop()
+    _handle = loop.call_later(0.5, cont)
 
 
 # persistent undo
