2dc4326064b81c6b3950583d11b60a01e00c35d5
Fly me to the moon <git@bigly.dog>
2020-12-30 01:31:03 -0800

    added operators

--- python/_registery.py
+++ python/_registery.py
@@ -1,5 +1,7 @@
 from .editor import auto_comp, auto_save, linter, prettier, search, syntax, whitespace
 from .modes import command, normal, poly, terminal, visual
+from .operators import move, replace, search, sort, unimpaired
+from .text_objects import entire, indent, line, word
 from .version_control import git
 from .workspace import input, misc, navigation, theme, wm
 
