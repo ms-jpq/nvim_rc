f83b0eab6c70a59f12b6c4c1fed3d770f93d7924
Fly me to the moon <git@bigly.dog>
2020-12-30 08:58:05 -0800

    term win

--- python/workspace/terminal.py
+++ python/workspace/terminal.py
@@ -1,13 +1,35 @@
+from typing import Sequence
+from uuid import uuid4
+
 from pynvim import Nvim
-from ..registery import keymap, rpc
+from pynvim.api.buffer import Buffer
+from pynvim.api.common import NvimError
 
 from ..nvim.float_win import open_float_win
+from ..registery import keymap, rpc
+
+BUF_VAR_NAME = f"buf_cursor_pos_{uuid4().hex}"
+
+
+def _single_term_buf(nvim: Nvim) -> Buffer:
+    bufs: Sequence[Buffer] = nvim.api.list_bufs()
+    for buf in bufs:
+        try:
+            nvim.api.buf_get_var(buf, BUF_VAR_NAME)
+        except NvimError:
+            pass
+        else:
+            return buf
+    else:
+        buf = nvim.api.create_buf(False, True)
+        nvim.api.buf_set_var(buf, BUF_VAR_NAME, BUF_VAR_NAME)
+        return buf
 
 
 @rpc(blocking=True)
 def open_floating(nvim: Nvim, *args: str) -> None:
-    buf = nvim.api.create_buf(False, True)
-    open_float_win(nvim, margin=1, relsize=0.9, buf=buf)
+    buf = _single_term_buf(nvim)
+    fw = open_float_win(nvim, margin=0, relsize=0.95, buf=buf)
 
 
 keymap.n("<leader>u") << f"<cmd>lua {open_floating.lua_name}()<cr>"
