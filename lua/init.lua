58747782c59d136fdfbb08af30271cbb60c76aed
Fly me to the moon <git@bigly.dog>
2020-12-31 04:15:25 -0800

    headless

--- lua/init.lua
+++ lua/init.lua
@@ -1,10 +1,10 @@
+local uis = vim.api.nvim_list_uis()
 local nvim_home = vim.api.nvim_list_runtime_paths()[1]
 local py_main = nvim_home .. "/init.py"
 
 
 local on_exit = function (_, code)
   vim.api.nvim_err_writeln("EXITED - " .. code)
-  local uis = vim.api.nvim_list_uis()
   if #uis == 0 then
     os.exit(code)
   end
@@ -19,8 +19,8 @@ local on_stderr = function (_, msg)
 end
 
 
-vim.fn.jobstart(
-  { py_main, vim.v.servername },
-  {   on_exit = on_exit,
-    on_stdout = on_stdout,
-    on_stderr = on_stderr })
+local args = { py_main, vim.v.servername }
+if #uis == 0 then
+  table.insert(args, "--headless")
+end
+vim.fn.jobstart(args, { on_exit = on_exit, on_stdout = on_stdout, on_stderr = on_stderr })
