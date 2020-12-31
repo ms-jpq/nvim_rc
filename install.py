4e6a8f9d3b26dbde871f06e25cd171deb6b917a3
Fly me to the moon <git@bigly.dog>
2020-12-30 22:34:06 -0800

    pkg inst

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ from asyncio.queues import Queue
 from asyncio.tasks import create_task, gather
 from os import linesep
 from sys import stderr
-from typing import Awaitable, Iterator, Sequence
+from typing import Awaitable, Iterator, Sequence, Tuple
 
 from std2.asyncio.subprocess import ProcReturn, call
 
@@ -29,7 +29,7 @@ def parse_args() -> Namespace:
     return parser.parse_args()
 
 
-async def pip(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
+async def pip(queue: Queue[Tuple[str, ProcReturn]], pkgs: Sequence[str]) -> None:
     if pkgs:
         p = await call(
             "pip3",
@@ -41,21 +41,21 @@ async def pip(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
             *pkgs,
             cwd=str(PIP_DIR),
         )
-        await queue.put(p)
+        await queue.put(("", p))
 
 
-async def npm(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
+async def npm(queue: Queue[Tuple[str, ProcReturn]], pkgs: Sequence[str]) -> None:
     if pkgs:
         p1 = await call("npm", "init", "--yes", cwd=str(NPM_DIR))
-        await queue.put(p1)
+        await queue.put(("", p1))
         if p1.code == 0:
             p2 = await call(
                 "npm", "install", "--upgrade", "--", *pkgs, cwd=str(NPM_DIR)
             )
-            await queue.put(p2)
+            await queue.put(("", p2))
 
 
-async def git(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
+async def git(queue: Queue[Tuple[str, ProcReturn]], pkgs: Sequence[str]) -> None:
     def it() -> Iterator[Awaitable[None]]:
         for pkg in pkgs:
 
@@ -65,7 +65,7 @@ async def git(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
                     p = await call(
                         "git", "pull", "--recurse-submodules", cwd=str(location)
                     )
-                    await queue.put(p)
+                    await queue.put((pkg, p))
                 else:
                     p = await call(
                         "git",
@@ -76,21 +76,21 @@ async def git(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
                         pkg,
                         str(location),
                     )
-                    await queue.put(p)
+                    await queue.put((pkg, p))
 
             yield cont(pkg)
 
     await gather(*it())
 
 
-async def bash(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
+async def bash(queue: Queue[Tuple[str, ProcReturn]], pkgs: Sequence[str]) -> None:
     def it() -> Iterator[Awaitable[None]]:
         for pkg in pkgs:
 
             async def cont(pkg: str) -> None:
                 stdin = f"set -x{linesep}{pkg}".encode()
                 p = await call("bash", stdin=stdin, cwd=str(TOP_LEVEL))
-                await queue.put(p)
+                await queue.put((pkg, p))
 
             if pkg:
                 yield cont(pkg)
@@ -98,13 +98,15 @@ async def bash(queue: Queue[ProcReturn], pkgs: Sequence[str]) -> None:
     await gather(*it())
 
 
-async def stdout(queue: Queue[ProcReturn]) -> None:
+async def stdout(queue: Queue[Tuple[str, ProcReturn]]) -> None:
     while True:
-        proc = await queue.get()
+        debug, proc = await queue.get()
         if proc.code == 0:
+            print(debug)
             print("✅ 👉", proc.prog, *proc.args)
             print(proc.out.decode())
         else:
+            print(debug, file=stderr)
             print(
                 f"⛔️ - {proc.code} 👉",
                 proc.prog,
@@ -117,7 +119,7 @@ async def stdout(queue: Queue[ProcReturn]) -> None:
 
 async def main() -> None:
     args = parse_args()
-    queue = Queue[ProcReturn]()
+    queue = Queue[Tuple[str, ProcReturn]]()
     tasks = gather(
         pip(queue, pkgs=args.pip),
         npm(queue, pkgs=args.npm),
