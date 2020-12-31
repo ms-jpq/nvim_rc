ada98cbcb39758efedd8ebb159c1311d05060c6c
Fly me to the moon <git@bigly.dog>
2020-12-30 23:36:14 -0800

    added search

--- python/operators/search.py
+++ python/operators/search.py
@@ -1,66 +1,67 @@
+from pynvim.api.buffer import Buffer
+from pynvim.api.nvim import Nvim
 
-# local magic_escape = function (word)
-#   local escaped = fn.escape(word, [[/\]])
-#   return [[\V]] .. string.gsub(escaped, "\n", [[\n]])
-# end
-
-
-# -- find selection
-# local find = function ()
-
-#   local hlselect = function (text)
-#     fn.setreg("/", magic_escape(text))
-#     bindings.exec[[set hlsearch]]
-#   end
-
-#   local selecthl = function (selc)
-#     local selection = p_selection(selc)
-#     hlselect(selection)
-#     return selection
-#   end
-
-#   lv.op_search = function (selc)
-#     selecthl(selc)
-#   end
-
-#   lv.op_fzf = function (selc)
-#     local selection = selecthl(selc)
-#     bindings.exec("BLines " .. selection)
-#   end
-
-#   lv.op_rg = function (selc)
-#     local selection = selecthl(selc)
-#     bindings.exec("Rg " .. selection)
-#   end
-
-#   bindings.map.normal("gs", "<cmd>set opfunc=v:lua.lv.op_search<cr>g@")
-
-#   bindings.map.normal("gf", "<cmd>set opfunc=v:lua.lv.op_fzf<cr>g@")
-#   bindings.map.normal("gF", "<cmd>set opfunc=v:lua.lv.op_rg<cr>g@")
-
-#   bindings.map.visual("gf", "<esc><cmd>lua lv.op_fzf()<cr>")
-#   bindings.map.visual("gF", "<esc><cmd>lua lv.op_rg()<cr>")
-
-# end
-# registry.defer(find)
+from ..nvim.operators import VisualTypes, escape, get_selected
+from ..registery import keymap, rpc
 
 
-# -- replace selection
-# local replace = function ()
+# search and highlight
+def _magic_escape(text: str) -> str:
+    rules = {"\\": "\\\\", "/": "\\/", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
+    return "".join(escape(text, escape=rules))
 
-#   lv.op_sd = function (selc)
-#     local selection = p_selection(selc)
-#     local escaped = magic_escape(selection)
-#     -- no magic
-#     local input = [[:%s/]] .. escaped .. "//g<left><left>"
-#     api.nvim_input(input)
-#   end
 
-#   -- no magic
-#   bindings.map.normal("gt", "<cmd>set opfunc=v:lua.lv.op_sd<cr>g@")
-#   bindings.map.visual("gt", "<ESC>:lua lv.op_sd()<cr>")
-#   -- very magic
-#   bindings.map.normal("gT", [[:%s/\v//g<left><left><left>]], {silent = false})
+def _hl_text(nvim: Nvim, text: str) -> None:
+    nvim.funcs.setreg("/", _magic_escape(text))
+    nvim.command("set hlsearch")
 
-# end
-# registry.defer(replace)
+
+def _hl_selected(nvim: Nvim, visual: VisualTypes) -> str:
+    buf: Buffer = nvim.api.get_current_buf()
+    selected = get_selected(nvim, buf=buf, visual_type=visual)
+    _hl_text(nvim, text=selected)
+    return selected
+
+
+@rpc(blocking=True)
+def _op_search(nvim: Nvim, visual: VisualTypes = None) -> None:
+    _hl_selected(nvim, visual=visual)
+
+
+@rpc(blocking=True)
+def _op_fzf(nvim: Nvim, visual: VisualTypes = None) -> None:
+    text = _hl_selected(nvim, visual=visual)
+    nvim.command(f"BLines {text}")
+
+
+@rpc(blocking=True)
+def _op_rg(nvim: Nvim, visual: VisualTypes = None) -> None:
+    text = _hl_selected(nvim, visual=visual)
+    nvim.command(f"Rg {text}")
+
+
+keymap.n("gs") << f"<cmd>set opfunc={_op_search.remote_name}<cr>g@"
+keymap.v("gs") << f"<esc><cmd>lua {_op_search.remote_name}()<cr>"
+
+keymap.n("gf") << f"<cmd>set opfunc={_op_fzf.remote_name}<cr>g@"
+keymap.v("gf") << f"<esc><cmd>lua {_op_fzf.remote_name}()<cr>"
+
+keymap.n("gF") << f"<cmd>set opfunc={_op_rg.remote_name}<cr>g@"
+keymap.v("gF") << f"<esc><cmd>lua {_op_rg.remote_name}()<cr>"
+
+
+# replace selection
+# no magic
+def _op_sd(nvim: Nvim, visual: VisualTypes = None) -> None:
+    buf: Buffer = nvim.api.get_current_buf()
+    selected = get_selected(nvim, buf=buf, visual_type=visual)
+    escaped = _magic_escape(selected)
+    instruction = f":%s/{escaped}//g<left><left>"
+    nvim.api.input(instruction)
+
+
+keymap.n("gt") << f"<cmd>set opfunc={_op_sd.remote_name}<cr>g@"
+keymap.v("gt") << f"<esc><cmd>lua {_op_sd.remote_name}()<cr>"
+
+# very magic
+keymap.n("gT", silent=False) << ":%s/\\v//g<left><left><left>"
