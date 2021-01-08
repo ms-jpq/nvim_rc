05275e053f2dad3bebf99f9161b7c9cc1379534a
Fly me to the moon <git@bigly.dog>
2021-01-08 11:19:01 +0700

    thread safe call

--- python/client.py
+++ python/client.py
@@ -2,15 +2,16 @@ from os import linesep
 
 from pynvim import Nvim
 from pynvim_pp.client import BasicClient
-from pynvim_pp.lib import async_call, write
+from pynvim_pp.lib import threadsafe_call, write
 
 from ._registery import ____
 from .components.install import maybe_install
 from .registery import drain
 
 
+
 class Client(BasicClient):
-    async def wait(self, nvim: Nvim) -> int:
+    def wait(self, nvim: Nvim) -> int:
         def init() -> None:
             atomic, specs = drain(nvim)
             self._handlers.update(specs)
@@ -18,5 +19,5 @@ class Client(BasicClient):
             # write(nvim, *atomic, sep=linesep, error=True)
             maybe_install(nvim)
 
-        await async_call(nvim, init)
-        return await super().wait(nvim)
+        threadsafe_call(nvim, init)
+        return super().wait(nvim)
