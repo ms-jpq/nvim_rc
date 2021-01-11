926beb2a65dbcce9517c09fffa1232778048dad4
Fly me to the moon <git@bigly.dog>
2021-01-11 20:02:19 +0700

    lua fmt

--- lua/init.lua
+++ lua/init.lua
@@ -1,19 +1,20 @@
 local nvim_home = vim.api.nvim_list_runtime_paths()[1]
 local py_main = nvim_home .. "/init.py"
 
-
-local on_exit = function (_, code)
+local on_exit = function(_, code)
   vim.api.nvim_err_writeln("EXITED - " .. code)
 end
 
-local on_stdout = function (_, msg)
+local on_stdout = function(_, msg)
   vim.api.nvim_out_write(table.concat(msg, "\n"))
 end
 
-local on_stderr = function (_, msg)
+local on_stderr = function(_, msg)
   vim.api.nvim_err_write(table.concat(msg, "\n"))
 end
 
-
-local args = { "python3", py_main, "--socket", vim.v.servername }
-vim.fn.jobstart(args, { on_exit = on_exit, on_stdout = on_stdout, on_stderr = on_stderr })
+local args = {"python3", py_main, "--socket", vim.v.servername}
+vim.fn.jobstart(
+  args,
+  {on_exit = on_exit, on_stdout = on_stdout, on_stderr = on_stderr}
+)
