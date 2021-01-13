d7f0c2681f193745c762ea2a857fe384596d8f36
Fly me to the moon <git@bigly.dog>
2021-01-14 01:40:30 +0700

    ok

--- python/operators/replace.py
+++ python/operators/replace.py
@@ -4,7 +4,6 @@ from pynvim_pp.api import (
     buf_set_lines,
     cur_buf,
     cur_window,
-    str_col_pos,
     win_get_cursor,
 )
 from pynvim_pp.operators import VisualTypes, operator_marks, writable
@@ -18,12 +17,12 @@ def _go_replace(nvim: Nvim, visual: VisualTypes = None) -> None:
     if not writable(nvim, buf=buf):
         return
     else:
-        (row1, c1), (row2, c2) = operator_marks(nvim, buf=buf, visual_type=visual)
+        (row1, col1), (row2, col2) = operator_marks(nvim, buf=buf, visual_type=visual)
 
         lines = buf_get_lines(nvim, buf=buf, lo=row1, hi=row2 + 1)
-        head = lines[0].encode()[:c1].decode()
+        head = lines[0].encode()[:col1].decode()
         body: str = nvim.funcs.getreg("*")
-        tail = lines[-1].encode()[c2 + 1 :].decode()
+        tail = lines[-1].encode()[col2 + 1 :].decode()
 
         new_lines = (head + body + tail).splitlines()
         line = new_lines.pop()
--- python/operators/search.py
+++ python/operators/search.py
@@ -1,7 +1,10 @@
+from os import linesep
+
+from pynvim.api import Buffer
 from pynvim.api.nvim import Nvim
-from pynvim_pp.api import cur_buf
+from pynvim_pp.api import buf_get_lines, cur_buf
 from pynvim_pp.lib import async_call, go
-from pynvim_pp.operators import VisualTypes, escape, get_selected
+from pynvim_pp.operators import VisualTypes, escape, operator_marks
 
 from ..registery import keymap, rpc
 
@@ -21,9 +24,22 @@ def _hl_text(nvim: Nvim, text: str) -> None:
     go(async_call(nvim, cont))
 
 
+def _get_selected(nvim: Nvim, buf: Buffer, visual_type: VisualTypes) -> str:
+    (row1, col1), (row2, col2) = operator_marks(nvim, buf=buf, visual_type=visual_type)
+    lines = buf_get_lines(nvim, buf=buf, lo=row1, hi=row2 + 1)
+
+    if len(lines) == 1:
+        return lines[0].encode()[col1 : col2 + 1].decode()
+    else:
+        head = lines[0].encode()[col1:].decode()
+        body = lines[1:-1]
+        tail = lines[-1].encode()[: col2 + 1].decode()
+        return linesep.join((head, *body, tail))
+
+
 def _hl_selected(nvim: Nvim, visual: VisualTypes) -> str:
     buf = cur_buf(nvim)
-    selected = get_selected(nvim, buf=buf, visual_type=visual)
+    selected = _get_selected(nvim, buf=buf, visual_type=visual)
     _hl_text(nvim, text=selected)
     return selected
 
@@ -60,7 +76,7 @@ keymap.v("gF") << f"<esc><cmd>lua {_op_rg.name}()<cr>"
 @rpc(blocking=True)
 def _op_sd(nvim: Nvim, visual: VisualTypes = None) -> None:
     buf = cur_buf(nvim)
-    selected = get_selected(nvim, buf=buf, visual_type=visual)
+    selected = _get_selected(nvim, buf=buf, visual_type=visual)
     escaped = _magic_escape(selected)
     instruction = f":%s/{escaped}//g<left><left>"
     nvim.api.input(instruction)
