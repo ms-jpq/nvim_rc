acef5c71111cf8b8d0adfb3a05acf639f2a7e2fc
Fly me to the moon <git@bigly.dog>
2021-05-15 05:00:00 -0700

    escp

--- python/operators/search.py
+++ python/operators/search.py
@@ -7,15 +7,21 @@ from pynvim.api.nvim import Nvim
 from pynvim_pp.api import buf_get_lines, cur_buf
 from pynvim_pp.lib import async_call, go
 from pynvim_pp.operators import VisualTypes, operator_marks
-from std2.lex import escape_with_replacement
+from std2.lex import escape_with_prefix
 
 from ..registery import keymap, rpc
 
 
 # search and highlight
 def _magic_escape(text: str) -> str:
-    rules = {"\\": "\\\\", "/": "\\/", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
-    return "".join(escape_with_replacement(text, escape=rules))
+    rules = {
+        "\\": "\\",
+        "/": "\\",
+        "\n": "\\",
+        "\r": "\\",
+        "\t": "\\",
+    }
+    return "".join(escape_with_prefix(text, escape=rules))
 
 
 def _hl_text(nvim: Nvim, text: str) -> None:
