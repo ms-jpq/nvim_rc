2ec26a6ab94bb1f3b8ec12e70fa49a529c6e99b9
Fly me to the moon <git@bigly.dog>
2021-05-19 17:21:53 -0700

    added cache

--- python/workspace/statusline.lua
+++ python/workspace/statusline.lua
@@ -1,16 +1,35 @@
 (function ()
-  local lsp_clients = function ()
-    local clients = vim.lsp.buf_get_clients(0)
-    local acc = {}
-    for _, client in ipairs(clients) do
-      local warnings = vim.lsp.diagnostic.get_count(0, "Warning", client.id)
-      local errors = vim.lsp.diagnostic.get_count(0, "Error", client.id)
-      table.insert(acc, {name=client.name, warnings=warnings, errors=errors})
+  local cache = function (f)
+    local ans = nil
+    local count = 0
+
+    return function ()
+      if count % 15 == 0 then
+        ans = f()
+      end
+      count = count + 1
+      return ans
     end
-    return acc
   end
 
-  LUA_status_line = function ()
-    return
+
+  local lsp = function ()
+    local clients = vim.lsp.buf_get_clients(0)
+    local names = {}
+    local warnings, errors = 0, 0
+
+    for _, client in ipairs(clients) do
+      warnings = warnings + vim.lsp.diagnostic.get_count(0, "Warning", client.id)
+      errors = errors + vim.lsp.diagnostic.get_count(0, "Error", client.id)
+      table.insert(names, client.name)
+    end
+
+    local s1 = #names ~= 0 and "[" .. table.concat(names, " ") .. "]" or ""
+    local s2 = warnings ~= 0 and " ⚠️  " .. warnings or ""
+    local s3 = errors ~= 0 and " ⛔️ " .. errors or ""
+
+    return s1 .. s2 .. s3
   end
+
+  LSP_status_line = cache(lsp)
 end)()
--- python/workspace/statusline.py
+++ python/workspace/statusline.py
@@ -9,14 +9,15 @@ _ql = "%q"
 _name = "%F"
 _modified = "%m"
 
+_lsp = "%{v:lua.LSP_status_line()}"
 _tabs = "%{&expandtab ? &tabstop .. 'S' : 'T'}"
 _ft = "%Y"
 _scroll = "%3p%%"
 _pos = "%4l:%-4c"
 
 _lhs = f"{_preview}{_ql}{_name}{_modified}"
-_rhs = f"{_tabs} | {_ft} | {_scroll} | {_pos} "
-_line = f"{_lhs}%={_rhs}"
+_rhs = f"{_lsp} | {_tabs} | {_ft} | {_scroll} | {_pos}"
+_line = f"{_lhs} %= {_rhs}"
 
 atomic.exec_lua(_status, ())
 atomic.set_option("statusline", _line)
