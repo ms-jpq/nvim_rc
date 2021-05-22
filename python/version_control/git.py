d0861e5ce07a3f5e9aef7891610c5090cad6928a
Fly me to the moon <git@bigly.dog>
2021-05-22 01:54:47 -0700

    lg

--- python/version_control/git.py
+++ python/version_control/git.py
@@ -1,3 +1,5 @@
+from shutil import which
+
 from pynvim import Nvim
 
 from ..registery import keymap, rpc
@@ -6,7 +8,8 @@ from ..workspace.terminal import open_term
 
 @rpc(blocking=True)
 def _lazygit(nvim: Nvim) -> None:
-    open_term(nvim, "lazygit", opts={"env": {"LC_ALL": "C"}})
+    lg = which("lazygit")
+    open_term(nvim, lg, opts={"env": {"LC_ALL": "C"}})
 
 
 keymap.n("<leader>U") << f"<cmd>silent! wa! | lua {_lazygit.name}()<cr>"
