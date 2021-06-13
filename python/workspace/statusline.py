22e719150e8106f4d3d9fb4cd217165258c2eb63
Fly me to the moon <git@bigly.dog>
2021-06-13 00:52:36 -0700

    linefeed

--- python/workspace/statusline.py
+++ python/workspace/statusline.py
@@ -12,11 +12,12 @@ _modified = "%m"
 _lsp = "%{v:lua.LSP_status_line()}"
 _ft = "%y"
 _tabs = "%{&expandtab ? &tabstop .. 'S' : 'T'}"
+_linefeed = "%{&fileformat}"
 _pos = "%5l:%-3c"
 _scroll = "%3p%%"
 
 _lhs = f"{_preview}{_ql}{_name}{_modified}"
-_rhs = f"{_lsp} | {_ft} {_tabs}{_pos}{_scroll}"
+_rhs = f"{_lsp} | {_ft} {_tabs} {_linefeed}{_pos}{_scroll}"
 _line = f"{_lhs} %= {_rhs}"
 
 atomic.exec_lua(_status, ())
