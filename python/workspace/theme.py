52c81922da1b3705e8a3bfa361a0a6da322a55e7
Fly me to the moon <git@bigly.dog>
2021-06-20 06:42:39 -0700

    zellner

--- python/workspace/theme.py
+++ python/workspace/theme.py
@@ -24,6 +24,7 @@ settings["cursorline"] = True
 # constant cursor styling
 settings["guicursor"] = ""
 
+atomic.command("colorscheme zellner")
 # light background
 settings["background"] = "light"
 
