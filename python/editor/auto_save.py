b31758760acdb9169810f6c7ec9bdea63cd73163
I love my dog <git@bigly.dog>
2021-06-21 03:47:31 -0700

    cmd

--- python/editor/auto_save.py
+++ python/editor/auto_save.py
@@ -1,11 +1,5 @@
-from asyncio.events import Handle, get_running_loop
-from typing import Optional
-
-from pynvim.api.nvim import Nvim
-from pynvim_pp.lib import async_call, go
-
 from ..consts import BACKUP_DIR
-from ..registery import autocmd, rpc, settings
+from ..registery import autocmd, settings
 
 # auto load changes
 settings["autoread"] = True
@@ -25,26 +19,10 @@ settings["backupskip"] = ""
 settings["backupdir"] = str(BACKUP_DIR)
 settings["backupext"] = ".bak"
 
-autocmd("BufLeave", "FocusLost", "VimLeavePre") << "silent! wa!"
-
-
-_handle: Optional[Handle] = None
-
-
-@rpc(blocking=True)
-def _smol_save(nvim: Nvim) -> None:
-    global _handle
-    if _handle:
-        _handle.cancel()
-
-    def cont() -> None:
-        go(async_call(nvim, nvim.command, "silent! wa!"))
-
-    loop = get_running_loop()
-    _handle = loop.call_later(0.5, cont)
-
-
-autocmd("CursorHold", "CursorHoldI") << f"lua {_smol_save.name}()"
+(
+    autocmd("BufLeave", "FocusLost", "VimLeavePre", "CursorHold", "CursorHoldI")
+    << "silent! wa!"
+)
 
 
 # persistent undo
@@ -53,3 +31,4 @@ settings["undofile"] = True
 settings["undolevels"] = 1000
 # maximum number lines to save for undo on a buffer reload
 settings["undoreload"] = 1000
+
