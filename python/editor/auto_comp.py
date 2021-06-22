d7549d0fcec888559125322e9a076da893791a4e
I love my dog <git@bigly.dog>
2021-06-21 22:18:21 -0700

    one theme

--- python/editor/auto_comp.py
+++ python/editor/auto_comp.py
@@ -1,41 +1,11 @@
-from pynvim.api.nvim import Nvim
-
-from ..registery import atomic, autocmd, keymap, rpc, settings
+from ..registery import keymap, settings
 
 # transparency
 settings["pumblend"] = 5
 
 
-@rpc(blocking=True)
-def _update_pumheight(nvim: Nvim) -> None:
-    lines: int = nvim.options["lines"]
-    nvim.options["pumheight"] = max(round(lines * 0.3), 15)
-
-
-atomic.exec_lua(f"{_update_pumheight.name}()", ())
-autocmd("VimResized") << f"lua {_update_pumheight.name}()"
-
-
-# dont spam suggestions menu
-settings["shortmess"] += "c"
 # dont follow tags
 settings["complete"] -= "i"
-# complete menu
-settings["completeopt"] = ("menuone", "preview", "noinsert", "noselect")
-
-
-# cancel comp
-keymap.i("<esc>", expr=True) << "pumvisible() ? '<c-e><esc>' : '<esc>'"
-keymap.i("<bs>", expr=True) << "pumvisible() ? '<c-e><bs>' : '<bs>'"
-
-
-# cua
-(
-    keymap.i("<cr>", expr=True)
-    << "pumvisible() ? (complete_info().selected == -1 ? '<c-e><cr>' : '<c-y>') : '<cr>'"
-)
-keymap.i("<tab>", expr=True) << "pumvisible() ? '<c-n>' : '<tab>'"
-keymap.i("<s-tab>", expr=True) << "pumvisible() ? '<c-p>' : '<bs>'"
 
 
 # previous
@@ -48,6 +18,4 @@ keymap.i("<c-l>") << "<c-x><c-l>"
 keymap.i("<c-f>") << "<c-x><c-f>"
 # omnifunc
 keymap.i("<c-o>") << "<c-x><c-o>"
-# userfunc
-keymap.nv("<c-space>") << ""
-keymap.i("<c-space>") << "<c-x><c-u>"
+
