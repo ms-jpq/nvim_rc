79db343b1508f73bc576b4803dde33c5d771d037
Fly me to the moon <git@bigly.dog>
2021-08-31 16:44:23 -0700

    venvbin

--- config/scripts/dl_sumneko_lua.sh
+++ config/scripts/dl_sumneko_lua.sh
@@ -4,6 +4,16 @@ set -eu
 set -o pipefail
 
 
+VENV="$PWD/lua-compile-venv"
+VENV_BIN="$VENV/bin"
+NINJA="$VENV_BIN/ninja"
+if [[ ! -x "$NINJA" ]]
+then
+  python3 -m venv "$VENV"
+  "$VENV_BIN/pip" install -- ninja
+fi
+
+
 if [[ -d "$LIB" ]]
 then
   cd "$LIB" || exit 1
@@ -17,15 +27,13 @@ fi
 
 if [[ ! -x "$BIN" ]]
 then
-  if [[ "$OS" = 'Darwin' ]]
-  then
-   (
-     cd "$LIB/3rd/luamake" || exit 1
-     ./compile/install.sh
-     cd ../.. || exit 1
-     ./3rd/luamake/luamake rebuild
-   )
+ (
+   export PATH="$VENV_BIN:$PATH"
+   cd "$LIB/3rd/luamake" || exit 1
+   ./compile/install.sh
+   cd ../.. || exit 1
+   ./3rd/luamake/luamake rebuild
+ )
 
-   ln --symbolic --force -- "$LIB/bin/"*'/lua-language-server' "$BIN"
-  fi
+ ln --symbolic --force -- "$LIB/bin/"*'/lua-language-server' "$BIN"
 fi
