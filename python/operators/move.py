cae1b6c40b33ded669569c54eb4646b5c0cce8e2
Fly me to the moon <git@bigly.dog>
2021-09-05 06:17:26 -0700

    mv without

--- python/operators/move.py
+++ python/operators/move.py
@@ -16,11 +16,12 @@ from ..registery import keymap, rpc
 def _norm_mv(nvim: Nvim, up: bool) -> None:
     win = cur_win(nvim)
     buf = win_get_buf(nvim, win=win)
+    row, _ = win_get_cursor(nvim, win=win)
+    lines = buf_line_count(nvim, buf=buf)
+
     if not writable(nvim, buf=buf):
         return
     else:
-        row, _ = win_get_cursor(nvim, win=win)
-        lines = buf_line_count(nvim, buf=buf)
         if up:
             if row:
                 nvim.command(f"move -2")
@@ -33,50 +34,42 @@ keymap.n("<m-up>") << f"<cmd>lua {_norm_mv.name}(true)<cr>"
 keymap.n("<m-down>") << f"<cmd>lua {_norm_mv.name}(false)<cr>"
 
 
-def _reselect_visual(nvim: Nvim) -> None:
-    nvim.command("norm! gv")
-
-
 @rpc(blocking=True)
-def _visual_up(nvim: Nvim) -> None:
+def _visual_mv(nvim: Nvim, up: bool) -> None:
     win = cur_win(nvim)
     buf = win_get_buf(nvim, win=win)
+    (row1, col1), (row2, col2) = operator_marks(nvim, buf=buf, visual_type=None)
+    lines = buf_line_count(nvim, buf=buf)
+
     if not writable(nvim, buf=buf):
         return
     else:
-        (row1, col1), (row2, col2) = operator_marks(nvim, buf=buf, visual_type=None)
-        if row1 <= 0:
-            _reselect_visual(nvim)
+        if up:
+            if row1 <= 0:
+                nvim.command("norm! gv")
+            else:
+                nvim.command(f"{row1+1},{row2+1}move {row1-1}")
+                set_visual_selection(
+                    nvim,
+                    win=win,
+                    mode="v",
+                    mark1=(row1 - 1, col1),
+                    mark2=(row2 - 1, col2),
+                )
+
         else:
-            curr = buf_get_lines(nvim, buf=buf, lo=row1, hi=row2 + 1)
-            nxt = buf_get_lines(nvim, buf=buf, lo=row1 - 1, hi=row1)
-            new = tuple((*curr, *nxt))
-            buf_set_lines(nvim, buf=buf, lo=row1 - 1, hi=row2 + 1, lines=new)
-            set_visual_selection(
-                nvim, win=win, mode="v", mark1=(row1 - 1, col1), mark2=(row2 - 1, col2)
-            )
+            if row2 >= lines - 1:
+                nvim.command("norm! gv")
+            else:
+                nvim.command(f"{row1+1},{row2+1}move {row2 +2}")
+                set_visual_selection(
+                    nvim,
+                    win=win,
+                    mode="v",
+                    mark1=(row1 + 1, col1),
+                    mark2=(row2 + 1, col2),
+                )
 
 
-@rpc(blocking=True)
-def _visual_down(nvim: Nvim) -> None:
-    win = cur_win(nvim)
-    buf = win_get_buf(nvim, win=win)
-    if not writable(nvim, buf=buf):
-        return
-    else:
-        (row1, col1), (row2, col2) = operator_marks(nvim, buf=buf, visual_type=None)
-        count = buf_line_count(nvim, buf=buf)
-        if row2 >= count - 1:
-            _reselect_visual(nvim)
-        else:
-            curr = buf_get_lines(nvim, buf=buf, lo=row1, hi=row2 + 1)
-            nxt = buf_get_lines(nvim, buf=buf, lo=row2 + 1, hi=row2 + 2)
-            new = tuple((*nxt, *curr))
-            buf_set_lines(nvim, buf=buf, lo=row1, hi=row2 + 2, lines=new)
-            set_visual_selection(
-                nvim, win=win, mode="v", mark1=(row1 + 1, col1), mark2=(row2 + 1, col2)
-            )
-
-
-keymap.v("<m-up>") << rf"<c-\><c-n><cmd>lua {_visual_up.name}()<cr>"
-keymap.v("<m-down>") << rf"<c-\><c-n><cmd>lua {_visual_down.name}()<cr>"
+keymap.v("<m-up>") << rf"<c-\><c-n><cmd>lua {_visual_mv.name}(true)<cr>"
+keymap.v("<m-down>") << rf"<c-\><c-n><cmd>lua {_visual_mv.name}(false)<cr>"
