3cb7f7db916aa850282469aa0c2316957d7a8fad
Fly me to the moon <git@bigly.dog>
2021-09-06 22:53:01 -0700

    undo

--- python/editor/auto_save.py
+++ python/editor/auto_save.py
@@ -21,7 +21,3 @@ settings["backupskip"] = ""
 
 # persistent undo
 settings["undofile"] = True
-# maximum number of changes that can be undone
-settings["undolevels"] = 1000
-# maximum number lines to save for undo on a buffer reload
-settings["undoreload"] = 1000
