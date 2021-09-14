25d035d9a54c3c5f8211a2f5572ea054dfce53b6
Fly me to the moon <git@bigly.dog>
2021-09-14 09:18:47 -0700

    mv

--- config/scripts/dl_texlab.sh
+++ config/scripts/dl_texlab.sh
@@ -4,16 +4,22 @@ set -eu
 set -o pipefail
 
 
+T_BIN="$(dirname "$BIN")/tectonic"
+
+
 if [[ "$OS" == 'Darwin' ]]
 then
   URI="$MAC_URI"
+  T_URI="$T_MAC_URI"
 else
   URI="$LINUX_URI"
+  T_URI="$T_LINUX_URI"
 fi
 
 
 TMP="$(mktemp --directory)"
 get -- "$URI" | unpack --dest "$TMP"
+get -- "$T_URI" | unpack --dest "$TMP"
 mv -- "$TMP/texlab" "$BIN"
-chmod +x -- "$BIN"
-
+mv -- "$TMP/tectonic" "$T_BIN"
+chmod +x -- "$BIN" "$T_BIN"
