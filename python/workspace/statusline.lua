f5bed1bccd0e556ed38ed25dd10a6377a4b9cd6d
Fly me to the moon <git@bigly.dog>
2021-09-16 17:31:10 -0700

    sort

--- python/workspace/statusline.lua
+++ python/workspace/statusline.lua
@@ -1,10 +1,10 @@
-(function ()
-  local cache = function (f)
+(function()
+  local cache = function(f)
     local ans = nil
     local w = 0
     local count = 0
 
-    return function ()
+    return function()
       local win = vim.api.nvim_get_current_win()
       if win ~= w or count % 15 == 0 then
         ans = f()
@@ -15,21 +15,26 @@
     end
   end
 
+  local sort = function(lhs, rhs)
+    return vim.stricmp(lhs, rhs) < 0
+  end
 
-  local lsp = function ()
+  local lsp = function()
     local clients = vim.lsp.buf_get_clients(0)
     local names = {}
     local warnings, errors = 0, 0
 
     for _, client in pairs(clients) do
-      warnings = warnings + vim.lsp.diagnostic.get_count(0, "Warning", client.id)
+      warnings =
+        warnings + vim.lsp.diagnostic.get_count(0, "Warning", client.id)
       errors = errors + vim.lsp.diagnostic.get_count(0, "Error", client.id)
       table.insert(names, client.name)
     end
 
-    local s1 = #names ~= 0 and "[" .. table.concat(names, " ") .. "]" or ""
-    local s2 = warnings ~= 0 and " ⚠️  " .. warnings or ""
-    local s3 = errors ~= 0 and " ⛔️ " .. errors or ""
+    table.sort(names, sort)
+    local s1 = #names > 0 and "[" .. table.concat(names, " ") .. "]" or ""
+    local s2 = warnings > 0 and " ⚠️  " .. warnings or ""
+    local s3 = errors > 0 and " ⛔️ " .. errors or ""
 
     return s1 .. s2 .. s3
   end
