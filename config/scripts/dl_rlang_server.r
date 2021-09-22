c70f41e4537d98edd8178034a6516e55911d352e
Fly me to the moon <git@bigly.dog>
2021-09-22 10:33:35 -0700

    rfmt

--- config/scripts/dl_rlang_server.r
+++ config/scripts/dl_rlang_server.r
@@ -1,10 +1,12 @@
 #!/usr/bin/env Rscript
 
-if (Sys.which("apt")) {
-  system2("apt", c("install", "--yes", "--", 
-                   "libcurl4-openssl-dev",
-                   "libssl-dev",
-                   "libxml2-dev"))
+if (Sys.which("apt") != "") {
+  system2("apt", c(
+    "install", "--yes", "--",
+    "libcurl4-openssl-dev",
+    "libssl-dev",
+    "libxml2-dev"
+  ))
 }
 
 if (!require(languageserver)) {
