85590c48d79c9618b23ac55fcdc25c3d8d39fe54
Fly me to the moon <git@bigly.dog>
2021-09-22 14:50:47 -0700

    lemminx uri

---
+++ config/scripts/dl/lemminx.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+shopt -s globstar failglob
+
+
+if [[ "$OS" == 'Darwin' ]]
+then
+  URI="$MAC_URI"
+else
+  URI="$LINUX_URI"
+fi
+
+
+TMP="$(mktemp --directory)"
+get -- "$URI" | unpack --dest "$TMP"
+mv -- "$TMP/lemminx"* "$BIN"
+chmod +x -- "$BIN"
