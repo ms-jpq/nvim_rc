afdafca4fd2f0bb81eb740f313e68db75500525a
Fly me to the moon <git@bigly.dog>
2021-09-30 16:33:38 -0700

    lg

--- python/version_control/git.py
+++ python/version_control/git.py
@@ -6,7 +6,7 @@ from ..workspace.terminal import open_term
 
 @rpc(blocking=True)
 def _lazygit(nvim: Nvim) -> None:
-    open_term(nvim, "lazygit", opts={"env": {"LC_ALL": "C"}})
+    open_term(nvim, "lazygit")
 
 
 keymap.n("<leader>U") << f"<cmd>silent! wa! | lua {NAMESPACE}.{_lazygit.name}()<cr>"
