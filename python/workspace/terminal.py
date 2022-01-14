f6824895ef56dbacf0ca96e2c3630ba057bdb9e0
IIIIII <github@bigly.dog>
2022-01-14 07:37:32 -0800

    ok

--- python/workspace/terminal.py
+++ python/workspace/terminal.py
@@ -77,7 +77,7 @@ def open_term(nvim: Nvim, *args: AnyPath, opts: TermOpts = {}) -> None:
         _term_open(nvim, *argv, opts=opts)
 
 
-atomic.command(f"command! -nargs=* FTerm lua {NAMESPACE}.{open_term.name}(<f-args>)")
+atomic.command(f"command! -nargs=* FCmd lua {NAMESPACE}.{open_term.name}(<f-args>)")
 
 
 @rpc(blocking=True)
