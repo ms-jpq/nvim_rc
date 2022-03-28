320f300a749291ac42956626bc363d1087225b7a
root <root@dev.guest.home.arpa>
2022-03-28 17:22:46 +0000

    ok

--- config/scripts/dl/rlang_server.r
+++ config/scripts/dl/rlang_server.r
@@ -9,10 +9,16 @@ if (apt != "") {
     "libcurl4-openssl-dev",
     "libssl-dev",
     "libxml2-dev"
-  ))
+  ), env = c("DEBIAN_FRONTEND=noninteractive"))
 }
 
+repos <- c("https://cloud.r-project.org")
+pkgs <- c("languageserver")
+
 if (!require(languageserver)) {
-  install.packages("languageserver")
-  library(languageserver)
+  install.packages(pkgs, repos = repos)
+} else {
+  update.packages(oldPkgs = pkgs, repos = repos)
 }
+
+library(languageserver)
