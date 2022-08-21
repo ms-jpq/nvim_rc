ece459321a89c21a1d90362a15236fb3ee12e171
IIIIII <git@bigly.dog>
2022-08-21 01:33:01 +0000

    ok

--- python/workspace/repl.py
+++ python/workspace/repl.py
@@ -1,16 +1,16 @@
-from dataclasses import dataclass
-from itertools import chain, count
+from itertools import count
 from math import inf
 from os import linesep
 from tempfile import NamedTemporaryFile
-from typing import Iterator, Optional, Sequence
+from textwrap import dedent
+from typing import Iterator, Optional
 from uuid import uuid4
 
 from pynvim import Nvim
 from pynvim.api.nvim import Buffer
 from pynvim_pp.api import (
     ExtMarkBase,
-    ask_mc,
+    ask,
     buf_commentstr,
     buf_del_extmarks,
     buf_get_extmarks_base,
@@ -25,29 +25,14 @@ from pynvim_pp.api import (
     win_get_buf,
     win_get_cursor,
 )
-from pynvim_pp.lib import async_call, decode, encode, go
+from pynvim_pp.lib import encode, go
 from pynvim_pp.operators import operator_marks
 from std2.asyncio.subprocess import call
 
-from ..registery import NAMESPACE, keymap, rpc
+from ..registery import LANG, NAMESPACE, keymap, rpc
 
 _NS = uuid4()
-_SEP = "\x1f"
 _TMUX_NS = "NVIM-"
-
-
-@dataclass(frozen=True, order=True)
-class _Pane:
-    session_name: str
-    window_index: int
-    window_name: str
-    pane_index: int
-    pane_title: str
-
-    uid: str
-
-    def __str__(self) -> str:
-        return f"S: {self.session_name} -> W: {self.window_index}:{self.window_name} -> {self.pane_index}:{self.pane_title}"
 
 
 @rpc(blocking=True)
@@ -59,6 +44,7 @@ def _marks_clear(nvim: Nvim, visual: bool) -> None:
         clear_ns(nvim, id=ns, buf=buf, lo=lo, hi=hi + 1)
     else:
         clear_ns(nvim, id=ns, buf=buf)
+        buf_set_var(nvim, buf=buf, key=str(_NS), val=None)
 
 
 _ = keymap.n("<leader>E") << f"<cmd>lua {NAMESPACE}.{_marks_clear.name}(false)<cr>"
@@ -78,13 +64,13 @@ def _mark_set(nvim: Nvim, visual: bool) -> None:
     indices = {mark.idx for mark in marks}
     gen = (i for i in count(1) if i not in indices)
     lcs, rcs = buf_commentstr(nvim, buf=buf)
-    cs = ((lcs + rcs) or "#") * 6
+    cs = (lcs + rcs) + LANG("repl")
 
     def mk(r: int) -> ExtMarkBase:
         return ExtMarkBase(
             idx=next(gen),
             begin=(r, 0),
-            meta={"virt_lines": (((cs, "NormalFloat"),),)},
+            meta={"virt_text": ((cs, "Normal"),), "virt_text_pos": "right_align"},
         )
 
     if visual:
@@ -122,74 +108,27 @@ _ = (
 )
 
 
-async def _ls_panes() -> Sequence[_Pane]:
-    proc = await call(
-        "tmux",
-        "list-panes",
-        "-a",
-        "-F",
-        _SEP.join(
-            (
-                "#{pane_id}",
-                "#{session_name}",
-                "#{window_index}",
-                "#{window_name}",
-                "#{pane_index}",
-                "#{pane_title}",
-            )
-        ),
-    )
-
-    def cont() -> Iterator[_Pane]:
-        for line in decode(proc.stdout).strip().splitlines():
-            (
-                pane_id,
-                session_name,
-                window_index,
-                window_name,
-                pane_index,
-                pane_title,
-            ) = line.split(_SEP)
-            pane = _Pane(
-                uid=pane_id,
-                session_name=session_name,
-                window_index=int(window_index),
-                window_name=window_name,
-                pane_index=int(pane_index),
-                pane_title=pane_title,
-            )
-            yield pane
-
-    return tuple(cont())
-
-
-async def _pane(nvim: Nvim, buf: Buffer) -> Optional[str]:
-    panes = sorted(await _ls_panes())
-
-    def cont() -> None:
-        answers = linesep.join(f"{idx} {pane}" for idx, pane in enumerate(panes))
-        answer_key = {k: v for k, v in enumerate(panes)}
-        pane = ask_mc(nvim, question="-->", answers=answers, answer_key=answer_key)
-        if pane:
-            buf_set_var(nvim, buf=buf, key=str(_NS), val=pane.uid)
-            return pane.uid
-        else:
-            return pane
-
-    await async_call(nvim, cont)
+def _pane(nvim: Nvim, buf: Buffer) -> Optional[str]:
+    if pane := ask(nvim, question=LANG("tmux pane?"), default="{last}"):
+        buf_set_var(nvim, buf=buf, key=str(_NS), val=pane)
+        return pane
+    else:
+        return None
 
 
 def _tmux_send(nvim: Nvim, buf: Buffer, text: str) -> None:
-    pane = buf_get_var(nvim, buf=buf, key=str(_NS))
+    pane: Optional[str] = buf_get_var(nvim, buf=buf, key=str(_NS)) or _pane(
+        nvim, buf=buf
+    )
 
     async def cont() -> None:
-        if t_pane := pane or await _pane(nvim, buf=buf):
+        if pane:
             name = f"{_TMUX_NS}-{uuid4()}"
             with NamedTemporaryFile() as fd:
                 fd.write(encode(text))
                 fd.flush()
                 await call("tmux", "load-buffer", "-b", name, "--", fd.name)
-                await call("tmux", "paste-buffer", "-d", "-r", "-b", name, "-t", t_pane)
+                await call("tmux", "paste-buffer", "-d", "-r", "-b", name, "-t", pane)
 
     go(nvim, aw=cont())
 
@@ -201,6 +140,7 @@ def _eval(nvim: Nvim, visual: bool) -> None:
 
     if visual:
         (begin, _), (end, _) = operator_marks(nvim, buf=buf, visual_type=None)
+        end += 1
     else:
         ns = create_ns(nvim, ns=_NS)
         row, _ = win_get_cursor(nvim, win=win)
@@ -214,7 +154,8 @@ def _eval(nvim: Nvim, visual: bool) -> None:
                 end = int(min(end or inf, r))
 
     lines = buf_get_lines(nvim, buf=buf, lo=begin, hi=end or -2 + 1)
-    _tmux_send(nvim, buf=buf, text=linesep.join(lines))
+    text = dedent(linesep.join(lines)) + linesep
+    _tmux_send(nvim, buf=buf, text=text)
 
 
 _ = keymap.n("<leader>g") << f"<cmd>lua {NAMESPACE}.{_eval.name}(false)<cr>"
