f277978b1628b991fabc0ca7f3c58565fd7b18ee
I Love My Dog <github@bigly.dog>
2022-10-11 16:49:28 +0000

    latex indent

--- config/scripts/dl/latex_format.sh
+++ config/scripts/dl/latex_format.sh
@@ -5,22 +5,24 @@ set -o pipefail
 shopt -s globstar failglob
 
 
-if [[ -d "$LIB" ]]
+if [[ ! -d "$LIB" ]]
 then
-  cd "$LIB" || exit 1
-  OPTS=(--recurse-submodules --no-tags)
-  git pull "${OPTS[@]}" origin "refs/tags/${TAG}:refs/tags/${TAG}"
-else
-  OPTS=(--depth=1 --recurse-submodules --shallow-submodules)
-  git clone "${OPTS[@]}" --branch "$TAG" "$URI" "$LIB"
+  TMP="$(mktemp --directory)"
+  TMP_LIB="$TMP/lib"
+  get -- "$URI" | unpack --dest "$TMP"
+  mv -- "$TMP"/* "$TMP_LIB"
+
+  PERL_LIB_DIR="$TMP_LIB/_perl_"
+  PERL_LIBS=(
+    YAML::Tiny
+    File::HomeDir
+    Unicode::GCString
+  )
+
+  cpanm --local-lib "$PERL_LIB_DIR" -- "${PERL_LIBS[@]}"
+
+  rm --recursive --force -- "$LIB"
+  mv -- "$TMP_LIB" "$LIB"
 fi
 
-PERL_LIB_DIR="$(dirname -- "$LIB")/perllibs"
-PERL_LIBS=(
-  YAML::Tiny
-  File::HomeDir
-  Unicode::GCString
-)
-
-cpanm --local-lib "$PERL_LIB_DIR" -- "${PERL_LIBS[@]}"
 cp -- "$(dirname -- "$0")/../exec/latexindent.sh" "$BIN"
