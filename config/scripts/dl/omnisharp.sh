17c8cc1980b51a1a48a5446518c56b6b0fb01bfa
I Love My Dog <github@bigly.dog>
2022-10-11 16:22:02 +0000

    fs

---
+++ config/scripts/dl/omnisharp.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -Eeu
+set -o pipefail
+shopt -s globstar failglob
+
+
+if [[ "$OSTYPE" =~ 'darwin' ]]
+then
+  URI="$MAC_URI"
+else
+  URI="$LINUX_URI"
+fi
+
+
+TMP="$(mktemp --directory)"
+get -- "$URI" | unpack --dest "$TMP"
+rm --recursive --force -- "$LIB"
+mv -- "$TMP" "$LIB"
+ln --symbolic --force -- "$LIB/OmniSharp" "$BIN"
