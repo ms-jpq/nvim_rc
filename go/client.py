afbc7c967a4cd926e7067d823f08f244cd6b60cf
IIIIII <git@bigly.dog>
2022-12-07 20:11:57 -0800

    client

--- go/client.py
+++ go/client.py
@@ -1,13 +1,12 @@
 from contextlib import AbstractAsyncContextManager
 from os import environ
-from pathlib import PurePath
 from time import time
 from types import NoneType
-from typing import Any, Literal, Sequence, Union
+from typing import Any, Sequence
 
 from pynvim_pp.logging import suppress_and_log
 from pynvim_pp.nvim import conn
-from pynvim_pp.rpc import MsgType
+from pynvim_pp.rpc import MsgType, ServerAddr
 from pynvim_pp.types import Method
 from std2.contextlib import nullacontext
 from std2.locale import si_prefixed_smol
@@ -33,9 +32,7 @@ async def _default(msg: MsgType, method: Method, params: Sequence[Any]) -> None:
         assert False, (msg, method, params)
 
 
-async def init(
-    socket: Union[PurePath, tuple[Literal["localhost"], int]], ppid: int
-) -> None:
+async def init(socket: ServerAddr, ppid: int) -> None:
     async with _suicide(ppid):
         async with conn(socket, default=_default) as client:
             atomic, handlers = drain()
