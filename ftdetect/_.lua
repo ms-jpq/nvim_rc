1bade9aa75ebffcd97a98ed6dc3c06a89ba809cf
IIIIII <git@bigly.dog>
2023-02-01 17:59:46 -0800

    faster re

--- ftdetect/_.lua
+++ ftdetect/_.lua
@@ -1,33 +1,31 @@
 vim.filetype.add {
   pattern = {
-    [".*"] = {
+    ["[^.]*"] = {
       priority = -math.huge,
       function(filename, bufnr)
-        local filetype = vim.filetype.match({filename = filename})
-        if filetype then
-          return filetype
+        local lhs = [[#!/usr/bin/env ]]
+        local l1 = unpack(vim.api.nvim_buf_get_lines(bufnr, 0, 1, true))
+        if not vim.startswith(l1, lhs) then
+          return
         else
-          local lhs = [[#!/usr/bin/env ]]
-          local l1 = vim.filetype.getlines(bufnr, 1)
-          if not vim.startswith(l1, lhs) then
-            return
-          else
-            local rhs = string.sub(l1, #lhs + 1)
-            local matches = {
-              sh = "(pw)@<!(ba|z)?sh",
-              python = "python3?",
-              perl = "perl",
-              ruby = "ruby",
-              r = "Rscript",
-              prolog = "swipl"
-            }
-            for ft, pattern in pairs(matches) do
-              local pat = [[\v^.{-}]] .. pattern .. ".{-}$"
-              if #vim.fn.matchstr(rhs, pat) > 1 then
-                return ft
-              end
+          local rhs = string.sub(l1, #lhs + 1)
+          local matches = {
+            sh = "(pw)@<!(ba|z)?sh",
+            python = "python3?",
+            perl = "perl",
+            ruby = "ruby",
+            r = "Rscript",
+            prolog = "swipl"
+          }
+
+          for ft, pattern in pairs(matches) do
+            local pat = [[\v^.{-}]] .. pattern .. ".{-}$"
+            if #vim.fn.matchstr(rhs, pat) > 1 then
+              return ft
             end
           end
+
+          return vim.filetype.match({filename = filename})
         end
       end
     }
