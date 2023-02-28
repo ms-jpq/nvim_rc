55ffa24b864e6289d06966c664af9ef281fb99bc
IIIIII <git@bigly.dog>
2023-02-28 15:10:04 -0800

    fmt2

--- config/scripts/exec/systemd-fmt.sh
+++ config/scripts/exec/systemd-fmt.sh
@@ -2,11 +2,11 @@
 
 readarray -t -d $'\n' -- LINES </dev/stdin
 
-MAX=1
+MAX=0
 for LINE in "${LINES[@]}"; do
   if [[ "$LINE" =~ ^[[:space:]]*# ]]; then
     true
-  elif [[ "$LINE" =~ ^[[:space:]]*([^=]+)= ]]; then
+  elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]+)[[:space:]]*= ]]; then
     M="${#BASH_REMATCH[1]}"
     MAX=$((M > MAX ? M : MAX))
   fi
@@ -19,13 +19,16 @@ for LINE in "${LINES[@]}"; do
     L="${#BASH_REMATCH[1]}"
     M=$((MAX - L))
 
-    P=""
+    P=" "
     for ((i = 0; i < M; i++)); do
       P="$P "
     done
 
     printf -- '%s\n' "${BASH_REMATCH[1]}${P}= ${BASH_REMATCH[2]}"
+  elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]*)[[:space:]]*$ ]]; then
+    printf -- '%s\n' "${BASH_REMATCH[1]}"
   else
-    printf -- '%s\n' "$LINE"
+    printf -- '%s\n' "$LINE" 2>&1
+    exit 1
   fi
 done
