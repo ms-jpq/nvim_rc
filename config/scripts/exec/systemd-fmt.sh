b9e56197c8ee4d03fef39275ee93681dd2f2c31b
IIIIII <git@bigly.dog>
2023-02-28 15:13:19 -0800

    fmt3

--- config/scripts/exec/systemd-fmt.sh
+++ config/scripts/exec/systemd-fmt.sh
@@ -4,17 +4,17 @@ readarray -t -d $'\n' -- LINES </dev/stdin
 
 MAX=0
 for LINE in "${LINES[@]}"; do
-  if [[ "$LINE" =~ ^[[:space:]]*# ]]; then
-    true
-  elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]+)[[:space:]]*= ]]; then
+  if [[ "$LINE" =~ ^[[:space:]]*#.*$ ]]; then
+    :
+  elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]+)[[:space:]]*=.*$ ]]; then
     M="${#BASH_REMATCH[1]}"
     MAX=$((M > MAX ? M : MAX))
   fi
 done
 
 for LINE in "${LINES[@]}"; do
-  if [[ "$LINE" =~ ^[[:space:]]*#(.*)$ ]]; then
-    printf -- '%s\n' "#${BASH_REMATCH[1]}"
+  if [[ "$LINE" =~ ^[[:space:]]*#[[:space:]]*(.*)$ ]]; then
+    printf -- '%s\n' "# ${BASH_REMATCH[1]}"
   elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]+)[[:space:]]*=[[:space:]]*(.*)[[:space:]]*$ ]]; then
     L="${#BASH_REMATCH[1]}"
     M=$((MAX - L))
@@ -28,7 +28,7 @@ for LINE in "${LINES[@]}"; do
   elif [[ "$LINE" =~ ^[[:space:]]*([^[:space:]]*)[[:space:]]*$ ]]; then
     printf -- '%s\n' "${BASH_REMATCH[1]}"
   else
-    printf -- '%s\n' "$LINE" 2>&1
+    printf -- '%s\n' "unexpected --> $LINE" 2>&1
     exit 1
   fi
 done
