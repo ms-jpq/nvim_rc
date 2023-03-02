64bb0bfc43c8b7f7e0fac2edfb071b96a5f1e6ab
IIIIII <git@bigly.dog>
2023-03-02 09:23:03 -0800

    more systemd

--- ftdetect/systemd.lua
+++ ftdetect/systemd.lua
@@ -1,7 +1,8 @@
 vim.filetype.add(
   {
     extension = {
-      nspawn = "systemd"
+      nspawn = "systemd",
+      service = "systemd"
     },
     pattern = {
       [".*/systemd/.*%.conf"] = "systemd"
