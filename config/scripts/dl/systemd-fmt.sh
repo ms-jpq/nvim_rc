0f812f17d93ea3d68ab08c5917957a39f50af236
IIIIII <git@bigly.dog>
2023-03-14 18:42:08 -0700

    fcp

--- config/scripts/dl/systemd-fmt.sh
+++ config/scripts/dl/systemd-fmt.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S -- bash -Eeuo pipefail -O dotglob -O failglob -O globstar
 
-cp -- "$(dirname -- "$0")/../exec/$(basename -- "$0")" "$BIN"
+cp --force -- "$(dirname -- "$0")/../exec/$(basename -- "$0")" "$BIN"
