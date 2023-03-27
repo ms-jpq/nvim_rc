8189cdc99763f50b240d8a537be4b84cba3cc7a0
IIIIII <git@bigly.dog>
2023-03-26 17:29:52 -0700

    export

--- config/scripts/dl/julia_ls.jl
+++ config/scripts/dl/julia_ls.jl
@@ -5,7 +5,7 @@ then
   BIN="$BIN.jl"
 fi
 
-export JULIA_DEPOT_PATH="$LIB/depot"
+export -- JULIA_DEPOT_PATH="$LIB/depot"
 mkdir --parents -- "$JULIA_DEPOT_PATH"
 cp -- "$(dirname -- "$0")/../exec/julia_ls.jl" "$BIN"
 exec -- julia --project="$LIB" "$0" "$@"
--- config/scripts/exec/julia_ls.jl
+++ config/scripts/exec/julia_ls.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S -- bash -Eeuo pipefail -O dotglob -O failglob -O globstar
 #=
 LIB="$(dirname -- "$0")/../lib/julia-ls"
-export JULIA_DEPOT_PATH="$LIB/depot"
+export -- JULIA_DEPOT_PATH="$LIB/depot"
 exec -- julia --project="$LIB" "$0" "$@"
 =#
 
