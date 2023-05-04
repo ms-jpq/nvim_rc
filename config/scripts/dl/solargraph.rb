1a5506539bc9107e280c8cebb9f1e7da7d799cff
root <root@dev.nspawn>
2023-05-04 13:51:12 -0700

    solargraph

---
+++ config/scripts/dl/solargraph.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S -- ruby
+# frozen_string_literal: true
+
+::Kernel.require('English')
+::Kernel.require('fileutils')
+
+if ::ENV.key?('INSTALL')
+  ::FileUtils.cp($PROGRAM_NAME, ::ENV.fetch('BIN'))
+else
+  ::ENV['SOLARGRAPH_CACHE'] = ::File.join(
+    ::ENV.fetch('XDG_CACHE_HOME'),
+    'solargraph',
+  )
+  ::Kernel.exec(
+    ::File.join(__dir__, '..', 'modules', 'rb_modules', 'bin', 'solargraph'),
+    *$ARGV,
+  )
+end
