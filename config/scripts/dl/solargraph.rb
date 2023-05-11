6a13abdc4c7b6e31215b20daa762019482118b79
IIIIII <git@bigly.dog>
2023-05-11 10:57:22 -0700

    w

--- config/scripts/dl/solargraph.rb
+++ config/scripts/dl/solargraph.rb
@@ -1,18 +1,15 @@
 #!/usr/bin/env -S -- ruby
 # frozen_string_literal: true
 
-::Kernel.require('English')
-::Kernel.require('fileutils')
+Kernel.require('English')
+Kernel.require('fileutils')
 
-if ::ENV.key?('INSTALL')
-  ::FileUtils.cp($PROGRAM_NAME, ::ENV.fetch('BIN'))
+if ENV.key?('INSTALL')
+  FileUtils.cp($PROGRAM_NAME, ENV.fetch('BIN'))
 else
-  ::ENV['SOLARGRAPH_CACHE'] = ::File.join(
-    ::ENV.fetch('XDG_CACHE_HOME'),
-    'solargraph',
-  )
-  ::Kernel.exec(
-    ::File.join(__dir__, '..', 'modules', 'rb_modules', 'bin', 'solargraph'),
+  ENV['SOLARGRAPH_CACHE'] = File.join(ENV.fetch('XDG_CACHE_HOME'), 'solargraph')
+  Kernel.exec(
+    File.join(__dir__, '..', 'modules', 'rb_modules', 'bin', 'solargraph'),
     *$ARGV,
   )
 end
