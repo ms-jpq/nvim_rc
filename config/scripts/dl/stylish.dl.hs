c04628f30627ed1fbd27eac477b1399d24781817
IIIIII <iiiiii@I6.lan>
2023-07-20 12:04:15 -0700

    p2

--- config/scripts/dl/stylish.dl.hs
+++ config/scripts/dl/stylish.dl.hs
@@ -1,9 +1,10 @@
 #!/usr/bin/env -S -- runhaskell
 
-import           System.Directory      (copyFileWithMetadata)
-import           System.Environment    (getEnv)
+import           System.Directory      (copyFileWithMetadata, getPermissions,
+                                        setOwnerExecutable, setPermissions)
+import           System.Environment    (getEnv, getExecutablePath)
 import           System.FilePath       ((</>))
-import           System.FilePath.Posix (takeBaseName)
+import           System.FilePath.Posix (dropExtension, takeBaseName)
 import           System.Info           (os)
 import           System.Process        (callProcess, readProcess)
 import           Text.Printf           (printf)
@@ -15,12 +16,19 @@ uri "darwin" = printf "%s-%s-darwin-x86_64.zip"     base
 uri "linux"  = printf "%s-%s-linux-x86_64.tar.gz" base
 link = uri os version
 
+nameof "linux" = dropExtension
+nameof _       = id
+
 main = do
   bin <- getEnv "BIN"
   tmp <- readProcess "mktemp" ["-d"] ""
-  let srv = tmp </> (takeBaseName link) </> "stylish-haskell"
+  let name = nameof os link
+  let srv = tmp </> (takeBaseName name) </> "stylish-haskell"
   tz <- readProcess "get.py" ["--", link] ""
   out <- readProcess "unpack.py" ["--dst", tmp] tz
   putStr out
+  self <- getExecutablePath
+  perms <- getPermissions self
+  _ <- setPermissions srv perms
   _ <- copyFileWithMetadata srv bin
   pure ()
