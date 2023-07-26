4c9dfd5547729c372008163f3e83e60b0024f8f3
IIIIII <github@bigly.dog>
2023-07-26 09:25:12 -0700

    ok6

--- config/scripts/dl/texlab.sh
+++ config/scripts/dl/texlab.sh
@@ -5,20 +5,21 @@ set -o pipefail
 T_BIN="$(dirname -- "$BIN")/tectonic"
 
 BASE='https://github.com/latex-lsp/texlab/releases/latest/download/texlab'
-T_BASE='https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%400.13.1/tectonic-0.13.1'
+T_BASE='https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%40'
+T_VERSION='0.14.1'
 
 case "$OSTYPE" in
 darwin*)
   URI="$BASE-aarch64-macos.tar.gz"
-  T_URI="$T_BASE-x86_64-apple-darwin.tar.gz"
+  T_URI="$T_BASE$T_VERSION/tectonic-$T_VERSION-x86_64-apple-darwin.tar.gz"
   ;;
 linux*)
   URI="$BASE-x86_64-linux.tar.gz"
-  T_URI="$T_BASE-x86_64-unknown-linux-gnu.tar.gz"
+  T_URI="$T_BASE$T_VERSION/tectonic-$T_VERSION-x86_64-unknown-linux-gnu.tar.gz"
   ;;
 *)
   URI="$BASE-x86_64-windows.zip"
-  T_URI="$T_BASE-x86_64-pc-windows-msvc.zip"
+  T_URI="$T_BASE$T_VERSION/tectonic-$T_VERSION-x86_64-pc-windows-msvc.zip"
   BIN="$BIN.exe"
   T_BIN="$T_BIN.exe"
   ;;
