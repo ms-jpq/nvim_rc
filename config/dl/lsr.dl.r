a1b930b9aaad3b12bce3644a16fbe6a615b22705
IIIIII <github@bigly.dog>
2023-07-27 01:55:01 -0700

    dont run R so much

--- config/dl/lsr.dl.r
+++ config/dl/lsr.dl.r
@@ -34,6 +34,10 @@ pkgs <- c("languageserver")
 dir.create(lib, recursive = TRUE)
 .libPaths(c(.libPaths(), lib))
 
+if (Sys.getenv("CI") != "" && sample(1:100, 1) >= c(10)) {
+  quit()
+}
+
 for (pkg in pkgs) {
   if (!require(pkg, lib.loc = c(lib), character.only = TRUE)) {
     install.packages(c(pkg), lib = c(lib), repos = repos)
