f95edcd1429569b79067ee848736f5a2e0b193e5
IIIIII <github@bigly.dog>
2023-08-01 21:17:59 -0700

    indent

--- config/dl/latexindent.ex.pl
+++ config/dl/latexindent.ex.pl
@@ -9,6 +9,6 @@ use utf8;
 my $dir = dirname(__FILE__);
 my $lib = "$dir/../lib/latexindent";
 
-$ENV{PERL5LIB} = "$lib/_perl_/lib/perl5";
+$ENV{PERL5LIB} = "$lib/perl/lib/perl5";
 exec "$lib/latexindent.pl", @ARGV;
 croak $ERRNO;
