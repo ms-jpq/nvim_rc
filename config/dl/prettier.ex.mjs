bfe58f0831ed2b349da4d7f594f7ed394a2e70bd
IIIIII <github@bigly.dog>
2023-08-03 06:01:08 -0700

    fmt
    fmt

--- config/dl/prettier.ex.mjs
+++ config/dl/prettier.ex.mjs
@@ -1,19 +1,40 @@
 #!/usr/bin/env -S -- node
 
-import { argv, exit } from "node:process";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
 import { spawnSync } from "node:child_process";
+import { dirname, join } from "node:path";
+import { argv, exit } from "node:process";
+import { fileURLToPath } from "node:url";
 
 const dir = dirname(fileURLToPath(import.meta.url));
-const bin = join(dirname(dir), "modules", "node_modules", ".bin", "prettier");
-const [, , ...args] = argv;
+const node_modules = join(dirname(dir), "modules", "node_modules");
+const bin = join(node_modules, ".bin", "prettier");
 
-// - --plugin=@prettier/plugin-xml
-// - --plugin=prettier-plugin-tailwindcss
-// - --plugin=prettier-plugin-organize-imports
+const [, , filetype, filename, tabsize] = argv;
 
-const { error, status, signal } = spawnSync(bin, args, { stdio: "inherit" });
+const plugins = (function* () {
+  if (filetype === "xml") {
+    yield join("@prettier", "plugin-xml");
+  }
+  if (filetype.match(/^(html|((java|type)scriptreact))$/)) {
+    yield join("prettier-plugin-tailwindcss", "dist", "index.js");
+  }
+  if (filetype.match(/^(java|type)script/)) {
+    yield join("prettier-plugin-organize-imports", "index.js");
+  }
+})();
+
+const args = (function* () {
+  yield `--stdin-filepath=${filename}`;
+  yield `--tab-width=${tabsize}`;
+  for (const plugin of plugins) {
+    yield `--plugin=${join(node_modules, plugin)}`;
+  }
+})();
+
+const { error, status, signal } = spawnSync(bin, [...args], {
+  stdio: "inherit",
+});
+
 if (error) {
   throw error;
 } else {
