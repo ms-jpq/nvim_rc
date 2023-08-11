78a10c824994066f12b02165f93d66609afd0e29
IIIIII <github@bigly.dog>
2023-08-11 13:30:22 -0700

    r2

--- config/dl/rubocop.ex.rb
+++ config/dl/rubocop.ex.rb
@@ -4,13 +4,19 @@
 ::Kernel.require('English')
 ::Kernel.require('Pathname')
 
-::Pathname.pwd.ascend.each do
-  gem = _1 / 'Gemfile'
-  if gem.exist?
-    Dir.chdir(_1)
-    ::Kernel.exec(*%w[bundle exec -- rubocop], *$ARGV)
+$ARGV => [filename, *argv]
+
+::Kernel
+  .Pathname(filename)
+  .parent
+  .ascend
+  .each do
+    gem = _1 / 'Gemfile'
+    if gem.exist?
+      Dir.chdir(_1)
+      ::Kernel.exec(*%w[bundle exec -- rubocop], *argv)
+    end
   end
-end
 
 cop = ::File.join(__dir__, *%w[.. modules rb_modules bin rubocop])
-::Kernel.exec(cop, *$ARGV)
+::Kernel.exec(cop, *argv)
