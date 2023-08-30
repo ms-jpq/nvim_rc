17658b64603996b3cbd4f775f3c4aec0541e90bf
IIIIII <github@bigly.dog>
2023-08-29 22:18:08 -0700

    ok

--- repl/m4.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-M4="$(</dev/stdin)"$'\n'
-
-printf -- '%q ' printf -- '%s' "$M4"
-printf -- '%s ' '|'
-printf -- '%q ' m4 --prefix-builtins
