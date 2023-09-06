36a53b19f857d68bd19545f5a5e0cf3ba662a55d
IIIIII <github@bigly.dog>
2023-09-05 22:05:45 -0700

    leader t

--- go/editor/lsp.py
+++ go/editor/lsp.py
@@ -28,8 +28,8 @@ _ = keymap.n("K") << "<cmd>lua vim.lsp.buf.hover()<cr>"
 _ = keymap.n("gm") << "<cmd>lua vim.lsp.buf.document_symbol()<cr>"
 _ = keymap.n("gM") << "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>"
 
-_ = keymap.n("<leader>d") << "<cmd>lua vim.diagnostic.setloclist()<cr>"
-_ = keymap.n("<leader>D") << "<cmd>lua vim.diagnostic.setqflist()<cr>"
+_ = keymap.n("<leader>t") << "<cmd>lua vim.diagnostic.setloclist()<cr>"
+_ = keymap.n("<leader>T") << "<cmd>lua vim.diagnostic.setqflist()<cr>"
 
 _ = (
     keymap.n("<c-p>")
