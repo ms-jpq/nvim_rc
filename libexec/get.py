ce928adb37904b6ae1f99a671ca19a4515c1712c
IIIIII <github@bigly.dog>
2023-09-09 11:39:25 -0700

    curl

--- libexec/get.py
+++ libexec/get.py
@@ -1,45 +1,34 @@
 #!/usr/bin/env -S -- PYTHONSAFEPATH= python3
 
 from argparse import ArgumentParser, Namespace
-from contextlib import suppress
-from datetime import timezone
-from email.utils import parsedate_to_datetime
-from http.client import HTTPResponse
-from io import DEFAULT_BUFFER_SIZE
-from os import utime
-from pathlib import Path, PurePosixPath
+from collections.abc import Iterator
+from pathlib import Path, PurePath, PurePosixPath
 from posixpath import normcase
+from shutil import which
+from subprocess import check_call
 from sys import stderr, stdout
-from tempfile import NamedTemporaryFile
 from textwrap import dedent
-from typing import Iterator, Tuple, cast
+from typing import Union
 from urllib.parse import unquote, urlsplit
-from urllib.request import Request, build_opener
-
-_OPEN = build_opener()
 
 
-def _meta(uri: str, timeout: float) -> Tuple[int, float]:
-    req = Request(url=uri, method="HEAD")
-    with _OPEN.open(req, timeout=timeout) as resp:
-        resp = cast(HTTPResponse, resp)
-        tot, mtime = 0, 0.0
-        for key, val in resp.headers.items():
-            match = key.casefold()
-            if match == "content-length":
-                tot = int(val)
-            elif match == "last-modified":
-                if req_mtime := parsedate_to_datetime(val):
-                    mtime = req_mtime.replace(tzinfo=timezone.utc).timestamp()
-
-        return tot, mtime
-
-
-def _fetch(uri: str, timeout: float) -> Iterator[bytes]:
-    with _OPEN.open(uri, timeout=timeout) as resp:
-        resp = cast(HTTPResponse, resp)
-        while buf := resp.read(DEFAULT_BUFFER_SIZE):
-            yield buf
+def _curl(
+    etag: PurePath, src: str, ttag: PurePath, tmp: PurePath, timeout: float
+) -> Iterator[Union[PurePath, str]]:
+    curl = which("curl")
+    assert curl
+    yield curl
+    yield from ("--fail", "--location", "--remote-time", "--no-progress-meter")
+    yield "--max-time"
+    yield str(timeout)
+    yield "--etag-compare"
+    yield etag
+    yield "--etag-save"
+    yield ttag
+    yield "--output"
+    yield tmp
+    yield "--"
+    yield src
 
 
 def _parse_args() -> Namespace:
@@ -53,9 +42,14 @@ def _parse_args() -> Namespace:
 def main() -> None:
     args = _parse_args()
 
-    src: str = args.src
+    src = str(args.src)
     path = PurePosixPath(unquote(urlsplit(src).path))
     dst = Path(args.dst if args.dst else path.name).resolve(strict=False)
+
+    etag = dst.with_name(f"{dst.name}.etag")
+    ttag = dst.with_name(f"{dst.name}.ttag")
+    tmp = dst.with_name(f"{dst.name}.tmp")
+    curl = tuple(_curl(etag, src=src, ttag=ttag, tmp=tmp, timeout=args.timeout))
 
     msg = f"""
     {src}
@@ -64,20 +58,14 @@ def main() -> None:
     """
     stderr.write(dedent(msg))
 
-    size, mtime = _meta(src, timeout=args.timeout)
-    with suppress(FileNotFoundError):
-        stat = dst.stat()
-        if size == stat.st_size and mtime == stat.st_mtime:
-            stdout.write(normcase(dst))
-            return
+    try:
+        check_call(curl)
+        tmp.rename(dst)
+        ttag.rename(etag)
+    finally:
+        tmp.unlink(missing_ok=True)
+        ttag.unlink(missing_ok=True)
 
-    stream = _fetch(src, timeout=args.timeout)
-    dst.parent.mkdir(parents=True, exist_ok=True)
-    with NamedTemporaryFile(dir=dst.parent, prefix=dst.name, delete=False) as fd:
-        fd.writelines(stream)
-
-    Path(fd.name).replace(dst)
-    utime(dst, (mtime, mtime))
     stdout.write(normcase(dst))
 
 
