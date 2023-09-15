7f3f7d7bc9a866b8f4713b0cc47793307402c325
iiiiii <github@bigly.dog>
2023-09-15 10:25:12 -0700

    remap

--- go/editor/lsp.py
+++ go/editor/lsp.py
@@ -28,8 +28,8 @@ _ = keymap.n("K") << "<cmd>lua vim.lsp.buf.hover()<cr>"
 _ = keymap.n("gm") << "<cmd>lua vim.lsp.buf.document_symbol()<cr>"
 _ = keymap.n("gM") << "<cmd>lua vim.lsp.buf.workspace_symbol()<cr>"
 
-_ = keymap.n("<leader>b") << "<cmd>lua vim.diagnostic.setloclist()<cr>"
-_ = keymap.n("<leader>B") << "<cmd>lua vim.diagnostic.setqflist()<cr>"
+_ = keymap.n("<leader>j") << "<cmd>lua vim.diagnostic.setloclist()<cr>"
+_ = keymap.n("<leader>J") << "<cmd>lua vim.diagnostic.setqflist()<cr>"
 
 _ = (
     keymap.n("<c-p>")
