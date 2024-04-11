b6c41810607ba3e6492e9fc1f42fbc0a47dd5e99
IIIIII <github@bigly.dog>
2024-04-10 22:57:29 -0700

    jdtls

--- config/dl/jdtls.java
+++ config/dl/jdtls.java
@@ -7,28 +7,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class jdtls {
   public static void main(String args[]) throws Exception {
+    final var win = System.getProperty("os.name").contains("Windows");
+
     final var uri =
         "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz";
     final var lib = Paths.get(System.getenv("LIB"));
     var bin = Paths.get(System.getenv("BIN"));
-    if (System.getProperty("os.name").contains("Windows")) {
+    if (win) {
       bin.resolveSibling("jdtls.bat");
     }
     final var src = lib.resolve("bin").resolve(bin.getFileName().toString());
     final var tmp = Paths.get(System.getenv("TMP"));
 
-    final var p1 =
-        new ProcessBuilder("env", "--", "get.sh", uri).redirectError(Redirect.INHERIT).start();
-    assert p1.waitFor() == 0;
-    final var dst = new String(p1.getInputStream().readAllBytes());
+    final var procs =
+        ProcessBuilder.startPipeline(
+            List.of(
+                new ProcessBuilder("env", "--", "get.sh", uri).redirectError(Redirect.INHERIT),
+                new ProcessBuilder("env", "--", "unpack.sh", tmp.toString())
+                    .redirectError(Redirect.INHERIT)));
 
-    final var p2 =
-        new ProcessBuilder("env", "--", "unpack.sh", tmp.toString(), dst).inheritIO().start();
-    assert p2.waitFor() == 0;
+    for (final var proc : procs) {
+      assert proc.waitFor() == 0;
+    }
 
     Files.deleteIfExists(bin);
     if (Files.exists(lib)) {
@@ -45,8 +50,12 @@ public class jdtls {
             throw new RuntimeException(e);
           }
         };
-    try (final var st = Files.walk(tmp)) {
-      st.forEach(cp);
+    if (win) {
+      try (final var st = Files.walk(tmp)) {
+        st.forEach(cp);
+      }
+    } else {
+      Files.move(tmp, lib);
     }
     Files.createSymbolicLink(bin, src);
   }
