298574b5120bb887f62c352e0413553b3c54ef68
IIIIII <github@bigly.dog>
2024-04-27 09:29:45 -0700

    more lsp

---
+++ config/dl/golangci.go
@@ -0,0 +1,46 @@
+// ; exec go run "$0" "$@"
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
+
+func main() {
+	lib, ok := os.LookupEnv("LIB")
+	if !ok {
+		log.Panicln()
+	}
+	bin, ok := os.LookupEnv("BIN")
+	if !ok {
+		log.Panicln()
+	}
+	if runtime.GOOS == "windows" {
+		bin += ".exe"
+	}
+
+	name := filepath.Base(bin)
+	dst := filepath.Join(lib, "bin", name)
+
+	cmd := exec.Command("go", "install", "--", "github.com/nametake/golangci-lint-langserver@latest")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = append(os.Environ(), "GO111MODULE=on", "GOPATH="+lib)
+
+	if err := cmd.Run(); err != nil {
+		log.Panicln(err)
+	}
+	if err := os.RemoveAll(bin); err != nil {
+		log.Panicln(err)
+	}
+	if err := os.Symlink(dst, bin); err != nil {
+		log.Panicln(err)
+	}
+}
