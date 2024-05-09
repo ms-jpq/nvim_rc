eec3e42136efcfe2b5ceb6b56056bfbd540ccc2b
hao <hao@stainlessapi.com>
2024-05-09 10:02:37 -0400

    nosort

--- config/dl/prettier.ex.js
+++ config/dl/prettier.ex.js
@@ -2,6 +2,7 @@
 
 import { ok } from "node:assert/strict"
 import { spawnSync } from "node:child_process"
+import { existsSync } from "node:fs"
 import { dirname, join } from "node:path"
 import { argv, execPath } from "node:process"
 import { fileURLToPath } from "node:url"
@@ -21,9 +22,13 @@ const plugins = {
   [join("@prettier", "plugin-xml", "src", "plugin.js")]: /^xml$/,
   [join("prettier-plugin-awk")]: /^awk$/,
   [join("prettier-plugin-nginx", "dist", "index.js")]: /^nginx$/,
-  [join("prettier-plugin-organize-imports", "index.js")]: /^(java|type)script/,
   [join("prettier-plugin-tailwindcss", "dist", "index.mjs")]:
     /^(html|((java|type)scriptreact))$/,
+}
+
+if (!existsSync("owo.nosort")) {
+  plugins[join("prettier-plugin-organize-imports", "index.js")] =
+    /^(java|type)script/
 }
 
 const args = (function* () {
