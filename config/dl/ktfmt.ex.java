8686f422550da3adc11e6ec023be5448e15c83b8
hao <hao@stainlessapi.com>
2024-05-20 12:46:24 -0400

    tabsize

--- config/dl/ktfmt.ex.java
+++ config/dl/ktfmt.ex.java
@@ -4,12 +4,17 @@ import java.util.stream.Stream;
 
 public class ktfmt {
   public static void main(String args[]) throws Exception {
+    final var tabsize = Integer.parseInt(args[0]);
     final var self = Path.of(System.getProperty("program.name"));
     final var java = Path.of(System.getProperty("java.home")).resolve("bin").resolve("java");
     final var jar =
         self.getParent().getParent().resolve("lib").resolve("ktfmt.java").resolve("ktfmt.jar");
     final var argv =
-        Stream.concat(Stream.of(java.toString(), "-jar", jar.toString()), Stream.of(args))
+        Stream.of(
+                Stream.of(java.toString(), "-jar", jar.toString()),
+                Stream.of(args).skip(1),
+                tabsize == 4 ? Stream.of("--dropbox-style") : Stream.of())
+            .flatMap(s -> s)
             .toArray(String[]::new);
 
     final var proc = new ProcessBuilder(argv).inheritIO().start();
