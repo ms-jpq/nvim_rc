c29b6eaac93853397264d2ba3ed76fd9e8e81456
IIIIII <github@bigly.dog>
2024-05-21 18:04:25 -0400

    use vim native comment

--- go/_registry.py
+++ go/_registry.py
@@ -2,7 +2,7 @@ from .editor import auto_comp, auto_save, code_action, linter, lsp, prettier
 from .editor import search as e_search
 from .editor import whitespace
 from .modes import command, normal, poly, terminal, visual
-from .operators import case, casing, comment, move, replace, search, sort
+from .operators import case, casing, move, replace, search, sort
 from .text_objects import entire, indent, line, word
 from .version_control import git
 from .workspace import (
@@ -26,7 +26,6 @@ assert case
 assert casing
 assert code_action
 assert command
-assert comment
 assert e_search
 assert entire
 assert git
