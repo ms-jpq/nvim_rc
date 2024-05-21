547deb248c514a6219815cc9df8de0bfa9af01db
IIIIII <github@bigly.dog>
2024-05-21 18:00:04 -0400

    diag move

--- go/modes/poly.py
+++ go/modes/poly.py
@@ -11,7 +11,7 @@ _ = keymap.nv("{") << ("5g<up>zz")
 _ = keymap.nv("}") << ("5g<down>zz")
 
 # re-center
-for key in ("n", "N", "[c", "]c"):
+for key in ("d", "n", "N", "[c", "]c"):
     _ = keymap.nv(key) << f"{key}zz"
 
 
