13fb277e6cb844a226cc63418a6e0f49e43b64f8
IIIIII <github@bigly.dog>
2024-05-21 22:18:07 -0400

    folding 2

--- go/workspace/fold.py
+++ go/workspace/fold.py
@@ -1,5 +1,9 @@
 from ..registry import keymap, settings
 
 _ = settings["foldlevel"] = 3
+# auto close folds
+_ = settings["foldclose"] = "all"
+
+_ = keymap.n("<leader>F", nowait=True) << "<cmd>set foldenable!<cr>"
 
 _ = keymap.n("a", nowait=True) << "za"
