ff48d510b38ea3297b727f39cb58fb2143c11d6a
IIIIII <github@bigly.dog>
2024-05-21 23:28:30 -0400

    fold2

--- go/workspace/fold.py
+++ go/workspace/fold.py
@@ -1,4 +1,11 @@
-from ..registry import autocmd, keymap, settings
+from uuid import uuid4
+
+from pynvim_pp.nvim import Nvim
+from pynvim_pp.window import Window
+
+from ..registry import NAMESPACE, autocmd, keymap, rpc, settings
+
+_BUF_VAR_NAME = f"buf_cursor_row_{uuid4().hex}"
 
 # use buffer text for folds
 settings["foldtext"] = ""
@@ -13,6 +20,24 @@ settings["foldopen"] += ("insert", "jump")
 # toggle folds
 _ = keymap.n("a", nowait=True) << "za"
 
-_ = autocmd("CursorMoved") << "silent! foldopen"
+
+@rpc()
+async def _open_fold() -> None:
+    win = await Window.get_current()
+    buf = await win.get_buf()
+    row, col = await win.get_cursor()
+    r = row + 1
+    lo = await Nvim.fn.foldclosed(int, r)
+    hi = await Nvim.fn.foldclosedend(int, r)
+    if lo == r:
+        await Nvim.exec(f"silent! foldopen")
+        if (prev := await buf.vars.get(int, _BUF_VAR_NAME)) and prev > row:
+            row = hi - 1
+            await win.set_cursor(row, col)
+    await buf.vars.set(_BUF_VAR_NAME, val=row)
+
+
+_ = autocmd("CursorMoved") << f"lua {NAMESPACE}.{_open_fold.method}()"
+
 _ = autocmd("InsertEnter") << "norm! zn"
 _ = autocmd("InsertLeave") << "norm! zN"
