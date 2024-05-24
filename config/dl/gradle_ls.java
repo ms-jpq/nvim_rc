8a5d637098691bca507b0434a1af2c0b7841829c
hao <hao@stainlessapi.com>
2024-05-24 11:33:43 -0400

    gradlels

--- config/dl/gradle_ls.java
+++ config/dl/gradle_ls.java
@@ -45,7 +45,7 @@ public class gradle_ls {
     final var cwd = tmp.resolve("vscode-gradle-" + version);
     final var built = cwd.resolve(name).resolve("build").resolve("install").resolve(name);
     final var p2 =
-        new ProcessBuilder(cwd.resolve("gradlew").toString(), "installDist")
+        new ProcessBuilder(cwd.resolve("gradlew") + (win ? ".bat" : ""), "installDist")
             .directory(cwd.toFile())
             .inheritIO()
             .start();
