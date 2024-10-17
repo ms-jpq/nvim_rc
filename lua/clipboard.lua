d5d941731b1cf6d017ab15cb6bb43a284d28c095
dogisgreat <github@bigly.dog>
2024-10-16 21:44:48 -0400

    -
    -

--- lua/clipboard.lua
+++ lua/clipboard.lua
@@ -1,8 +1,7 @@
 local esc = "\027"
 local tmux = vim.env.TMUX
 
-local osc52 = function(clipboard, str)
-  local b = vim.base64.encode(str)
+local osc52 = function(data)
   local acc = {}
 
   -- TMUX wrap start
@@ -16,12 +15,10 @@ local osc52 = function(clipboard, str)
   end
 
   -- OSC52 start
-  table.insert(acc, esc .. "]52;")
-  table.insert(acc, clipboard)
-  table.insert(acc, ";")
+  table.insert(acc, esc .. "]52;c;")
 
   -- OSC52 body
-  table.insert(acc, b)
+  table.insert(acc, data)
 
   -- TMUX escape `esc`
   if tmux then
@@ -39,20 +36,18 @@ local osc52 = function(clipboard, str)
   return table.concat(acc, "")
 end
 
-local copy = function(reg)
-  local clipboard = reg == "+" and "c" or "p"
-  return function(lines)
-    local s = table.concat(lines, "\n")
-    vim.api.nvim_chan_send(2, osc52("c", s))
-    if tmux then
-      vim.system({"tmux", "load-buffer", "--", "-"}, {stdin = lines})
-    end
+local copy = function(lines)
+  local s = table.concat(lines, "\n")
+  local b = vim.base64.encode(s)
+  vim.api.nvim_chan_send(2, osc52(b))
+  if tmux then
+    vim.system({"tmux", "load-buffer", "--", "-"}, {stdin = lines})
   end
 end
 
 local paste = function(reg)
   return function()
-    -- vim.api.nvim_chan_send(2, osc52(clipboard, s))
+    vim.api.nvim_chan_send(2, osc52("?"))
     return vim.split("", "\n", {plain = true})
   end
 end
@@ -62,8 +57,8 @@ vim.g.clipboard = {
   name = "OSC 52",
   cache_enabled = true,
   copy = {
-    ["+"] = copy("+"),
-    ["*"] = copy("*")
+    ["+"] = copy,
+    ["*"] = copy
   },
   paste = {
     ["+"] = paste("+"),
