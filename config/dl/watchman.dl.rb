f7a89d4c9d500ef0deb962c6a7816520ba0a166f
dogisgreat <git@bigly.dog>
2025-04-12 20:51:16 -0400

    watchman

--- config/dl/watchman.dl.rb
+++ config/dl/watchman.dl.rb
@@ -6,25 +6,22 @@ require('fileutils')
 require('open3')
 require('pathname')
 
-prefix = 'https://github.com/facebook/watchman/releases/download/'
 tmp, dst = ENV.fetch('TMP'), ENV.fetch('BIN')
 bin = Pathname(dst).parent
 
-postfix =
-  case [RUBY_PLATFORM, RUBY_PLATFORM]
-  in [/linux/, /x86_64/]
-    'v2025.03.24.00/watchman-v2025.03.24.00-linux.zip'
-  in [/darwin/, _]
-    exit
-  else
-    'v2025.02.24.00/watchman-v2025.02.24.00-windows.zip'
+case [RUBY_PLATFORM, RUBY_PLATFORM]
+in [/linux/, /x86_64/]
+  argv = %w[sudo -- apt install --no-install-recommends -- yes -- watchman]
+  system(*argv, exception: true)
+in [/darwin/, _]
+  exit
+else
+  uri = 'https://github.com/facebook/watchman/releases/download/v2025.02.24.00/watchman-v2025.02.24.00-windows.zip'
+  stats = Open3.pipeline(['env', '--', 'get.sh', uri], ['env', '--', 'unpack.sh', tmp])
+
+  raise unless stats.all?(&:success?)
+
+  Pathname.glob("#{tmp}/*/bin/*").each do
+    FileUtils.mv(_1, bin / _1.basename, verbose: true)
   end
-
-uri = prefix + postfix
-stats = Open3.pipeline(['env', '--', 'get.sh', uri], ['env', '--', 'unpack.sh', tmp])
-
-raise unless stats.all?(&:success?)
-
-Pathname.glob("#{tmp}/*/bin/*").each do
-  FileUtils.mv(_1, bin / _1.basename, verbose: true)
 end
