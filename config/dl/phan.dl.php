ad39103a55be8dfc571d8aa051cb326e7f44b048
dogisgreat <git@bigly.dog>
2025-05-13 13:38:23 -0400

    phan

--- config/dl/phan.dl.php
+++ config/dl/phan.dl.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 $uri = "https://github.com/phan/phan/releases/latest/download/phan.phar";
 
-$lib = getenv("LIB");
 $bin = getenv("BIN");
-assert($lib && $bin);
+assert($bin);
 
 $output = [];
 $code = -1;
@@ -14,19 +13,7 @@ exec(join(" ", array_map("escapeshellarg", ["get.sh", $uri])), $output, $code);
 assert($code === 0, join(PHP_EOL, $output));
 $file = join(PHP_EOL, $output);
 
-if (!is_dir($lib)) {
-  assert(mkdir($lib, 0755, true));
-}
-
-$basename = basename($file);
-assert(copy($file, "$lib/$basename"));
-
-if (PHP_OS_FAMILY === "Windows") {
-  $bin .= ".php";
-}
-
-$dir = dirname(__FILE__);
-assert(copy("{$dir}/phan.ex.php", $bin));
+assert(copy($file, $bin));
 assert(chmod($bin, 0755));
 
 
