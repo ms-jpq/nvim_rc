7813996f225030da0628c914aec2fc57efda2e59
dogisgreat <git@bigly.dog>
2025-05-13 13:23:04 -0400

    pslam

---
+++ config/dl/psalm.dl.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S -- php
+<?php
+declare(strict_types=1);
+
+$uri = "https://github.com/vimeo/psalm/releases/latest/download/psalm.phar";
+
+$bin = getenv("BIN");
+assert($bin);
+
+$output = [];
+$code = -1;
+exec(join(" ", array_map("escapeshellarg", ["get.sh", $uri])), $output, $code);
+assert($code === 0, join(PHP_EOL, $output));
+$file = join(PHP_EOL, $output);
+
+assert(copy($file, $bin));
+assert(chmod($bin, 0755));
+
+?>
