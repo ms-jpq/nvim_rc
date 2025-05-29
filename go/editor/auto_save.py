8a241a596e58202d124b6de01003e386815c57cf
dogisgreat <github@bigly.dog>
2025-05-29 00:37:24 -0400

    k

--- go/editor/auto_save.py
+++ go/editor/auto_save.py
@@ -45,7 +45,7 @@ _ = (
 
 async def _check_times() -> None:
     while True:
-        await sleep(0.5)
+        await sleep(0.6)
         with suppress_and_log():
             info = cast(Mapping[str, str], await Nvim.api.get_mode(NoneType))
             mode = info["mode"]
